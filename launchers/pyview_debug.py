253c078d608b015bb72905877aafe991860e96e7
jirhiker <jirhiker@gmail.com>
2014-01-22 18:24:52 -0700

    pyview launch fix

--- launchers/pyview_debug.py
+++ launchers/pyview_debug.py
@@ -22,7 +22,7 @@ ETSConfig.toolkit = "qt4"
 import os
 #============= local library imports  ==========================
 
-version_id = '_view'
+version_id = ''
 from helpers import build_version
 '''
     set_path=True inserts the pychron source directory into the PYTHONPATH
@@ -30,7 +30,7 @@ from helpers import build_version
     Use false (default) if your are launching from a standalone bundle. 
 '''
 DEBUG = True
-build_version(version_id, debug=DEBUG)
+build_version(version_id, setup_ver='_view',debug=DEBUG)
 
 def main():
     '''
