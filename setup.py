ef59d66c18fb4ad4db8de8dfc4421e1825fedfcb
jirhiker <jirhiker@gmail.com>
2014-03-04 14:59:36 -0700

    updated regression tests

--- setup.py
+++ setup.py
@@ -2,6 +2,7 @@ from setuptools import setup
 
 setup(name='pychron',
       version='2.0.4',
-      packages=['pychron.core.regression', ],
+      packages=['pychron.core.regression',
+                'pychron.core.regression.tests'],
       test_suite='pychron.core.regression.tests.suite'
 )
