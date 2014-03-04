63eb88372f5bdd6b07c44b756481f114e665791a
jirhiker <jirhiker@gmail.com>
2014-03-04 15:51:16 -0700

    updated setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(name='pychron',
       version='2.0.4',
       setup_requires=['nose>=1.0'],
-      py_modules=['pychron_constants'],
+      py_modules=['pychron.pychron_constants'],
       packages=[
           'pychron.core.regression',
                 'pychron.core.stats',
