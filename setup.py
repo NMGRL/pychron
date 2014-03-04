cd54a21b1f41d7424d3c6ea6f79cb5efd6218b0d
jirhiker <jirhiker@gmail.com>
2014-03-04 16:40:59 -0700

    unittest test

--- setup.py
+++ setup.py
@@ -4,8 +4,7 @@ setup(name='pychron',
       version='2.0.4',
       setup_requires=['nose>=1.0'],
       py_modules=['pychron.pychron_constants'],
-      packages=[
-          'pychron.core.regression',
+      packages=['pychron.core.regression',
                 'pychron.core.stats',
                 'pychron.core.regression.tests'],
       test_suite='pychron.core.regression.tests.suite'
