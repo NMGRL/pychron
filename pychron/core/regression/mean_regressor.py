f26b8bdfd06c5836d6f506385c9da0c20ca1bd45
jirhiker <jirhiker@gmail.com>
2014-03-14 12:20:28 -0600

    fixed MeanRegressor std

--- pychron/core/regression/mean_regressor.py
+++ pychron/core/regression/mean_regressor.py
@@ -73,9 +73,10 @@ sem={}
             ddof=0 provides a maximum likelihood estimate of the variance for normally distributed variables
             ddof=1 unbiased estimator of the variance of the infinite population
         """
-        if len(self.clean_ys) > self.ddof:
+        ys = self.clean_ys
+        if len(ys) > self.ddof:
             # ys = asarray(self.ys, dtype=float64)
-            return self.ys.std(ddof=self.ddof)
+            return ys.std(ddof=self.ddof)
         else:
             return 0
 
