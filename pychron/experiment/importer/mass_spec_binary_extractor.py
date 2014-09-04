6f4ee0a143b631cb651ac95a381918d6b08d13fa
jirhiker <jirhiker@gmail.com>
2014-09-04 17:11:06 -0600

    updated mass spec binary extractor

--- pychron/experiment/importer/mass_spec_binary_extractor.py
+++ pychron/experiment/importer/mass_spec_binary_extractor.py
@@ -1,4 +1,4 @@
-#===============================================================================
+# ===============================================================================
 # Copyright 2013 Jake Ross
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -20,10 +20,12 @@ import os
 import struct
 #============= standard library imports ========================
 #============= local library imports  ==========================
+from datetime import datetime
 from pychron.experiment.importer.extractor import Extractor
 
+
 class BinarySpec(object):
-    sample =''
+    pass
 
 
 class MassSpecBinaryExtractor(Extractor):
@@ -82,9 +84,7 @@ class MassSpecBinaryExtractor(Extractor):
         if not os.path.isfile(p):
             return
 
-        isref, isblank = False, False
-
-        bs=[]
+        bs = []
         with open(p, 'r') as fp:
             while 1:
                 try:
@@ -96,19 +96,21 @@ class MassSpecBinaryExtractor(Extractor):
         return bs
 
     def _import_analysis(self, fp):
+        isref, isblank = False, False
+
         bspec = BinarySpec()
 
         # use big-endian
         gns = self._get_next_str(fp)
 
-        gh = self._get_short(fp)
-        gs = self._get_single(fp)
-        gl = self._get_long(fp)
+        gshort = self._get_short(fp)
+        gsingle = self._get_single(fp)
+        glong = self._get_long(fp)
 
-        end_rec = gns()
+        bspec.runid = gns()
 
-        ms_data_file_ver=7.875
-        if ms_data_file_ver<7.875:
+        ms_data_file_ver = 7.875
+        if ms_data_file_ver < 7.875:
             ignored_rid = gns()
 
         bspec.sample = gns()
@@ -117,38 +119,41 @@ class MassSpecBinaryExtractor(Extractor):
         bspec.project = gns()
         bspec.locality = gns()
         bspec.rundate = gns()
+        bspec.runday = self._calculate_days(bspec.rundate)
         bspec.irradlabel = gns()
         bspec.fits = gns()
         bspec.comment = gns()
-        bspec.runhour = gs()
+        bspec.runhour = gsingle()
 
-        gof1, gof1 = gs(), gs()
-        emv = [gs()]
-        optemp = gs()
+        gof1, gof1 = gsingle(), gsingle()
+        emv = [gsingle()]
+        bspec.optemp = gsingle()
 
-        bspec.version= ver = gs()
+        bspec.version = ver = gsingle()
 
-        return bspec
-        # if ver > 4.96:
-        #     emv.append(gs())
-        #
-        # if ver >= 4.6:
-        #     history = gns()
-        #
-        # if ver >= 2:
-        #     s1 = gs()
-        # else:
-        #     s1 = 10 ^ 10
-        # scalefactor = s1, s1
-        #
-        # extract_device = ''
-        # if ver >= 4.46:
-        #     extract_device = gns()
-        #
-        # extract_value = gs()
-        # fsp = extract_value
-        # if ver > 3.1:
-        #     fsp = gs()  # final set power
+        if ver > 4.96:
+            emv.append(gsingle())
+
+        bspec.emv = emv
+
+        if ver >= 4.6:
+            bspec.history = gns()
+
+        if ver >= 2:
+            s1 = gsingle()
+        else:
+            s1 = 0  #10  10
+
+        bspec.scalefactor = s1, s1
+        bspec.extract_device = gns() if ver >= 4.46 else ''
+
+        extract_value = gsingle()
+        fsp = extract_value
+        if ver > 3.1:
+            fsp = gsingle()  # final set power
+
+        bspec.extract_value = extract_value  #power achieved
+        bspec.final_set_power = fsp  #power requested
         #
         # if extract_value == 0:
         #     if fsp > 0:
@@ -156,157 +161,218 @@ class MassSpecBinaryExtractor(Extractor):
         #     elif not (isref or isblank):
         #         extract_value = float(comment)
         #
-        # if ver > 4.34:
-        #     totdur_heating = gl()
-        #     todur_atsetpoint = gl()
-        #
-        # if ver > 2.32:
-        #     gain = [gs()]
-        #
-        # if ver > 4.96:
-        #     gain.append(gs())
-        #
-        # calc_with_ratio = bool(gh())
-        # system_number = 1
-        # if ver > 2.33:
-        #     mol_ref_iso = gs()
-        #     mol_ref_iso_special = mol_ref_iso < 0
-        #     mol_ref_iso = abs(mol_ref_iso)
-        #
-        #     system_number = gh()
-        #
-        # disc_source = 3
-        # if ver > 2.992:
-        #     disc = gs()
-        #     disc_err = gs()
-        #     j = gs()
-        #     jerr = gs()
-        #     disc_source = 2
-        #
-        # resistor_values = [1, 1, 10 ^ 11]
-        # if ver > 4.02:
-        #     resistor_values[2] = gs()
-        #     resistor_values[0] = gs()
-        #     if ver > 4.96:
-        #         resistor_values[1] = gs()
-        #     if isref:
-        #         mol_ref_iso = gs()
-        #
-        # isogrp = 'Ar'
-        # if ver > 6.67 and ver < 7.245:
-        #     isogrp = gns()
-        # elif ver >= 7.245:
-        #     dum = gh()
-        # elif ver > 2.93 and ver < 6.67:
-        #     grps = 'He,Ar,Ne,Kr,Xe'.split(',')
-        #
-        #     # Mass Spec is 1-indexed
-        #     idx = gh() - 1
-        #
-        #     isogrp = grps[idx]
-        #
-        # niso = 5
-        # if ver > 2.96:
-        #     niso = gh()
-        #
-        # nratio = 4
-        # if ver > 2.98:
-        #     nratio = gh()
-        #
-        # if ver > 2.92:
-        #     if ver < 3.1:
-        #         _, x = gs(), gs()
-        #
-        #     dets = []
-        #     for i in range(niso):
-        #         dt = gh()
-        #         if dt == 0:
-        #             dt = 1
-        #         dets.append(dt)
-        # else:
-        #     dets = [1, ] * niso
-        #
-        # ndet = 1
-        # if ver >= 3.81:
-        #     ndet = gh()
-        #     if ndet > 1:
-        #         mx_det_type_n = 1
-        #         #                     det_conv_factor = []
-        #         for i in range(mx_det_type_n):
-        #             for j in range(i, mx_det_type_n, 1):
-        #                 gs()
-        #
-        # elif ver >= 3.1:
-        #     ndet = gh()
-        #     if ndet > 1:
-        #         for i in range(ndet):
-        #             for j in range(ndet):
-        #                 gs()
-        #
-        # refdetnum = 1
-        # if ver > 4.02:
-        #     refdetnum = gh() or 1
-        # if ver > 2.994 and ver < 3.1:
-        #     x = gs()
-        #
-        # signormfactor = 1
-        # if ver > 4:
-        #     signormfactor = gs()
-        #
-        # ncyc = -1
-        # if ver > 2.994:
-        #     ncyc = gh()
-        #
-        # if refdetnum == 16256:
-        #     refdetnum = 1
-        #     signnormfactor = 1
-        #     ncyc = -1
-        #
-        # if ver > 2.994:
-        #     isokeys = []
-        #     for i in range(niso + nratio):
-        #         t = gns().replace(' ', '')
-        #         isokeys.append(t)
-        #     if ver > 3.121 and isref:
-        #         if isokeys[1] == 'Ar40':
-        #             isokeys[1] = 'Ar39'
-        #         if isokeys[2] == 'Ar40':
-        #             isokeys[2] = 'Ar38'
-        #     if ver < 6 and isokeys[0].index('40'):
-        #         isokeys[0] = 'Ar40'
-        #
-        #     numisokeys = []
-        #     demisokeys = []
-        #     for i in range(nratio):
-        #         numisokeys.append(gh())
-        #         demisokeys.append(gh())
-        # else:
-        #     niso = 5
-        #     nratio = 4
-        #     isokeys = ['Ar40', 'Ar39', 'Ar38', 'Ar37', 'Ar36',
-        #                'Ar40/Ar39',
-        #                'Ar38/Ar39',
-        #                'Ar37/Ar39',
-        #                'Ar36/Ar39',
-        #                'Bsln'
-        #     ]
-        #     numisokeys = [0, 2, 3, 4]
-        #     demisokeys = [1, 1, 1, 1]
-        #     if isref:
-        #         isokeys[5] = 'Ar40/Ar36'
-        #         isokeys[6] = 'Ar40/Ar38'
-        #         numisokeys = [1, 1, None, None]
-        #         demisokeys = [5, 3, None, None]
-        #
-        # ncnts = []
-        # isobsln = []
-        # for i in range(niso):
-        #     ncnts.append(gs())
-        #     v = self._load_signal(fp)
-        #     isobsln.append(v)
+        bspec.totdur_heating = glong() if ver > 4.34 else 0
+        bspec.totdur_atsetpoint = glong() if ver > 4.34 else 0
 
+        gain = []
+        if ver > 2.32:
+            gain.append(gsingle())
 
-    def _load_signal(self, fp):
-        pass
+        if ver > 4.96:
+            gain.append(gsingle())
+
+        bspec.gain = gain
+
+        bspec.calc_with_ratio = bool(gshort())
+
+        system_number = 1
+        mol_ref_iso = 0
+        if ver > 2.33:
+            mol_ref_iso = gsingle()
+            mol_ref_iso_special = mol_ref_iso < 0
+            mol_ref_iso = abs(mol_ref_iso)
+
+            system_number = gshort()
+
+        bspec.mol_ref_iso = mol_ref_iso
+        bspec.system_number = system_number
+
+        disc_source = 3
+        disc = 0
+        disc_err = 0
+        j = 0
+        j_err = 0
+        if ver > 2.992:
+            disc = gsingle()
+            disc_err = gsingle()
+            j = gsingle()
+            j_err = gsingle()
+            disc_source = 2
+
+        bspec.disc = disc
+        bspec.disc_err = disc_err
+        bspec.j = j
+        bspec.j_err = j_err
+
+        resistor_values = [1, 1, 10 ** 11]
+        if ver > 4.02:
+            resistor_values[2] = gsingle()
+            resistor_values[0] = gsingle()
+            if ver > 4.96:
+                resistor_values[1] = gsingle()
+            if isref:
+                mol_ref_iso = gsingle()
+
+        bspec.resistor_values = resistor_values
+        isogrp = 'Ar'
+        if ver > 6.67 and ver < 7.245:
+            isogrp = gns()
+        elif ver >= 7.245:
+            dum = gshort()
+        elif ver > 2.93 and ver < 6.67:
+            grps = 'He,Ar,Ne,Kr,Xe'.split(',')
+
+            # Mass Spec is 1-indexed
+            idx = gshort() - 1
+
+            isogrp = grps[idx]
+
+        bspec.isogrp = isogrp
+
+        niso = gshort() if ver > 2.96 else 5
+        nratio = gshort() if ver > 2.98 else 4
+
+        if ver > 2.92:
+            if ver < 3.1:
+                _, x = gsingle(), gsingle()
+
+            dets = []
+            for i in range(niso):
+                dt = gshort()
+                if dt == 0:
+                    dt = 1
+                dets.append(dt)
+        else:
+            dets = [1, ] * niso
+
+        bspec.detectors = dets
+        ndet = 1
+        if ver >= 3.81:
+            ndet = gshort()
+            if ndet > 1:
+                mx_det_type_n = 1
+                #                     det_conv_factor = []
+                for i in range(mx_det_type_n):
+                    for j in range(i, mx_det_type_n, 1):
+                        gsingle()
+
+        elif ver >= 3.1:
+            ndet = gshort()
+            if ndet > 1:
+                for i in range(ndet):
+                    for j in range(ndet):
+                        gsingle()
+
+        # bspec.detector_ics =
+        bspec.ndet = ndet
+
+        bspec.refdetnum = gshort() or 1 if ver > 4.02 else 1
+
+        if 2.994 < ver < 3.1:
+            gsingle()
+
+        bspec.signormfactor = gsingle() if ver > 4 else 1
+
+        bspec.ncyc = gshort() if ver > 2.994 else -1
+
+        if ver > 2.994:
+            isokeys = []
+            for i in range(niso + nratio):
+                t = gns().replace(' ', '')
+                isokeys.append(t)
+            if ver > 3.121 and isref:
+                if isokeys[1] == 'Ar40':
+                    isokeys[1] = 'Ar39'
+                if isokeys[2] == 'Ar40':
+                    isokeys[2] = 'Ar38'
+
+            if ver < 6 and isokeys[0].index('40'):
+                isokeys[0] = 'Ar40'
+
+            numisokeys = []
+            demisokeys = []
+            for i in range(nratio):
+                numisokeys.append(gshort())
+                demisokeys.append(gshort())
+
+        else:
+            niso = 5
+            nratio = 4
+            isokeys = ['Ar40', 'Ar39', 'Ar38', 'Ar37', 'Ar36',
+                       'Ar40/Ar39',
+                       'Ar38/Ar39',
+                       'Ar37/Ar39',
+                       'Ar36/Ar39',
+                       'Bsln']
+            numisokeys = [0, 2, 3, 4]
+            demisokeys = [1, 1, 1, 1]
+            if isref:
+                isokeys[5] = 'Ar40/Ar36'
+                isokeys[6] = 'Ar40/Ar38'
+                numisokeys = [1, 1, None, None]
+                demisokeys = [5, 3, None, None]
+
+        bspec.niso = niso
+        bspec.nratio = nratio
+        bspec.isokeys = isokeys
+
+        ncnts = []
+        isobsln = []
+        for i in range(niso):
+            ncnts.append(int(gshort()))
+
+        isotopes = []
+        for i in range(niso):
+            iso, v = self._extract_isotope(ver, bspec.runday, ncnts[i], gsingle)
+            isotopes.append(iso)
+            isobsln.append(v)
+        bspec.ncnts=ncnts
+        bspec.isotopes=isotopes
+        return bspec
+
+    def _extract_isotope(self, ver, runday, ncnts, gsingle):
+        """
+        gns: get_next_string
+        gh: get_short
+        gs: get_single
+        gl: get_long
+        """
+
+        isodict = {'background': gsingle(),
+                   'background_err': gsingle(),
+                   'intercept': gsingle(),
+                   'intercept_err': gsingle()}
+        xs, ys = self._extract_data(ncnts, gsingle)
+        isodict['xs']=xs
+        isodict['ys']=ys
+        isodict['peak_height_change_percent'] = gsingle() if ver > 2.18 else 0
+        bsln_n = 0
+        c = -1
+        if ver > 2.994:
+            bsln_n = gsingle()
+            if runday<=543:
+                bsln_n=0
+            c = gsingle()
+
+        isodict['counts_per_cycle'] = c
+
+        if runday>545.67 and ver>1.03:
+            bsln_n=max(bsln_n, 1)
+        return isodict, bsln_n
+
+    def _extract_data(self, ncnts, gs):
+        xs,ys=[],[]
+        for i in xrange(ncnts):
+            ys.append(gs())
+            xs.append(gs())
+
+        return xs,ys
+
+    def _calculate_days(self, rundate_str):
+        d=datetime.strptime(rundate_str, '%m/%d/%Y')
+        refdate=datetime(year=1987, month=1, day=1)
+
+        return (d-refdate).total_seconds()/86400.
+
 
 #============= EOF =============================================
--- pychron/experiment/tests/binary_extractor.py
+++ pychron/experiment/tests/binary_extractor.py
@@ -6,16 +6,50 @@ import unittest
 
 
 class Expected(object):
-    project='Zimmerer'
+    runid = '59273-01A'
+    project = 'Zimmerer'
     sample = 'ORGAN-8'
     material = 'Biotite'
     investigator = 'Zimmerer'
     fits = 'LLLLLL'
-    locality=''
+    locality = ''
     rundate = '3/3/2010'
-    irradlabel='NM-227L'
+    irradlabel = 'NM-227L'
     runhour = 2.0
     version = 7.875
+    emv = [1.4503029584884644, 0.0]
+    optemp = 0
+    history = 'Multiplier Baseline = L @ --- @ ---; Ar40 = L @ --- @ ---; Ar39 = L @ --- @ ---; ' \
+              'Ar38 = L @ --- @ ---; Ar36 = L @ --- @ ---; Ar37 = L @ --- @ ---; ' \
+              'argonlab - 3/11/2010 @ 10:08:42 AM @ --- @ ---; Ar40 bk val,er = Bracketing blanks @ --- @ ---; ' \
+              'Ar39 bk val,er = Bracketing blanks @ --- @ ---; Ar38 bk val,er = Bracketing blanks @ --- @ ---; ' \
+              'Ar36 bk val,er = Bracketing blanks @ --- @ ---; Ar37 bk val,er = Bracketing blanks @ --- @ ---'
+    scalefactor=1000000000.0, 1000000000.0
+    extract_device='CO2'
+    extract_value=0.0
+    final_set_power=0.5
+    totdur_heating = 0
+    totdur_atsetpoint=0
+    gain = [166124.90625, 0.0]
+    calc_with_ratio = False
+    system_number = 3
+    mol_ref_iso = 8.004109832880028e-16
+    disc=1.00600004196167
+    disc_err=0.0010000000474974513
+    j=0.002384300110861659
+    j_err=2.048499936790904e-06
+    resistor_values =[1.0,0,0]
+    isogrp = 'Ar'
+    niso = 5
+    nratio=0
+    detectors=[1,1,1,1,1]
+    ndet = 1
+    refdetnum=1
+    signormfactor=1
+    ncyc=0
+    isokeys=['Ar40', 'Ar39', 'Ar38', 'Ar36', 'Ar37']
+    runday=8462.0
+    ncnts=[48, 72, 24, 120, 24]
 
 class MassSpecBinaryExtractorTestCase(unittest.TestCase):
     @classmethod
@@ -23,6 +57,9 @@ class MassSpecBinaryExtractorTestCase(unittest.TestCase):
         cls.extractor = MassSpecBinaryExtractor()
         cls.specs = cls.extractor.import_file('./data/MSDataFile_7875')
         cls.expected = Expected()
+
+    def test_runid(self):
+        self._test_spec_attr('runid')
 
     def test_sample(self):
         self._test_spec_attr('sample')
@@ -49,13 +86,100 @@ class MassSpecBinaryExtractorTestCase(unittest.TestCase):
         self._test_spec_attr('fits')
 
     # def test_comment(self):
-    #     self._test_spec_attr('comment', '1350; 1800/20')
+    # self._test_spec_attr('comment', '1350; 1800/20')
 
     def test_runhour(self):
         self._test_spec_attr('runhour')
 
     def test_version(self):
         self._test_spec_attr('version')
+
+    def test_optemp(self):
+        self._test_spec_attr('optemp')
+
+    def test_emv(self):
+        self._test_spec_attr('emv')
+
+    def test_history(self):
+        self._test_spec_attr('history')
+
+    def test_scalefactor(self):
+        self._test_spec_attr('scalefactor')
+
+    def test_extract_device(self):
+        self._test_spec_attr('extract_device')
+
+    def test_extract_value(self):
+        self._test_spec_attr('extract_value')
+
+    def test_final_set_power(self):
+        self._test_spec_attr('final_set_power')
+
+    def test_totdur_heating(self):
+        self._test_spec_attr('totdur_heating')
+
+    def test_totdur_atsetpoint(self):
+        self._test_spec_attr('totdur_atsetpoint')
+
+    def test_gain(self):
+        self._test_spec_attr('gain')
+
+    def test_calc_with_ratio(self):
+        self._test_spec_attr('calc_with_ratio')
+
+    def test_system_number(self):
+        self._test_spec_attr('system_number')
+
+    def test_mol_ref_iso(self):
+        self._test_spec_attr('mol_ref_iso')
+
+    def test_disc(self):
+        self._test_spec_attr('disc')
+
+    def test_disc_err(self):
+        self._test_spec_attr('disc_err')
+
+    def test_j(self):
+        self._test_spec_attr('j')
+
+    def test_j_err(self):
+        self._test_spec_attr('j_err')
+
+    def test_resistor_values(self):
+        self._test_spec_attr('resistor_values')
+
+    def test_isogrp(self):
+        self._test_spec_attr('isogrp')
+
+    def test_niso(self):
+        self._test_spec_attr('niso')
+
+    def test_nratio(self):
+        self._test_spec_attr('nratio')
+
+    def test_detectors(self):
+        self._test_spec_attr('detectors')
+
+    def test_ndet(self):
+        self._test_spec_attr('ndet')
+
+    def test_refdet_num(self):
+        self._test_spec_attr('refdetnum')
+
+    def test_signormfactor(self):
+        self._test_spec_attr('signormfactor')
+
+    def test_ncyc(self):
+        self._test_spec_attr('ncyc')
+
+    def test_isokeys(self):
+        self._test_spec_attr('isokeys')
+
+    def test_runday(self):
+        self._test_spec_attr('runday')
+
+    def test_ncnts(self):
+        self._test_spec_attr('ncnts')
 
     def _test_spec_attr(self, attr, idx=0):
         self.assertEqual(getattr(self.specs[idx], attr),
