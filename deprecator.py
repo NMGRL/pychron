c614ac2b74fa27a8a4139690a89e14e464e4faac
jirhiker <jirhiker@gmail.com>
2014-12-20 21:20:24 -0700

    removed deprecated

--- deprecator.py
+++ deprecator.py
@@ -1,11 +1,11 @@
 # ===============================================================================
-# Copyright 2013 Jake Ross
+# Copyright 2014 Jake Ross
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
-#   http://www.apache.org/licenses/LICENSE-2.0
+# http://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
@@ -18,21 +18,36 @@
 
 # ============= standard library imports ========================
 # ============= local library imports  ==========================
+import os
 
 
-#class MultiDevice(AbstractDevice):
-#    def load_additional_args(self, config):
-#        super(MultiDevice, self).load_additional_args(config)
-#
-#        """
-#            load devices
-#        """
-#        for sect in config.sections():
-#            if sect.startswith('Device '):
-#                self._add_device(config,sect)
-#
-#    def _add_device(self):
-#        self.application.register_service(type(d), )
+def locate_deprecated(root):
+    i=1
+    cnt=0
+    dcnt=0
+    for r, ds, fs in os.walk(root):
+        dcnt+=1
+        for fi in fs:
+            if not fi.endswith('.py') or fi == '__init__.py':
+                continue
+
+            cnt+=1
+            p = os.path.join(r, fi)
+            with open(p, 'r') as fp:
+                for li in fp:
+                    li = li.strip()
+                    if li and not li.startswith('#'):
+                        break
+                else:
+                    print '{:03n} is deprecated {}'.format(i, p)
+                    i+=1
+
+    print 'examined {} files from {} directories'.format(cnt, dcnt)
+
+locate_deprecated('/Users/ross/Programming/git/pychron_dev/pychron')
 
 
 # ============= EOF =============================================
+
+
+
