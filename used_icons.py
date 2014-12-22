a728ca012f36ab33fc609d10d7b636490ab851ba
jirhiker <jirhiker@gmail.com>
2014-12-21 22:36:13 -0700

    updated icon_requirements concept

--- used_icons.py
+++ used_icons.py
@@ -21,8 +21,8 @@
 import os
 import re
 
-matcher = re.compile(r"icon\('\w+(\.png)*'\)")
-
+icon_matcher = re.compile(r"icon\('\w+(\.png)*'\)")
+ibe_matcher = re.compile(r"icon_button_editor\([\'\(\)\w]+, *\'\w+(\.png)*'")
 
 def locate_used_icons(root):
     def func():
@@ -31,6 +31,7 @@ def locate_used_icons(root):
         dcnt = 0
         lcnt = 0
         for r, ds, fs in os.walk(root):
+            # print r
             dcnt += 1
             for fi in fs:
                 if not fi.endswith('.py') or fi == '__init__.py':
@@ -45,20 +46,31 @@ def locate_used_icons(root):
                         if li.startswith('#'):
                             continue
                         else:
-                            g = matcher.search(li)
-                            if g:
-                                m = g.group(0)
-                                if m in found:
-                                    continue
-                                else:
-                                    found.append(m)
-                                    yield dcnt, cnt, lcnt, m
+                            # for mat in (icon_matcher, ibe_matcher):
+                            for mat in (icon_matcher, ibe_matcher):
+                                g = mat.search(li)
+                                if g:
+                                    m = g.group(0)
+                                    if m.startswith('icon_button_editor'):
+                                        name=m.split(',')[-1].strip()[1:-1]
+                                    else:
+                                        name = m[6:-2]
+                                    if name in found:
+                                        continue
+                                    else:
+                                        found.append(name)
+                                        yield dcnt, cnt, lcnt, m, name
+                                        break
 
-    with open('icon_requirements.txt', 'w') as fp:
-        for di, ci, lcnt, mm in func():
-            name = mm[6:-2]
-            print mm, name
-            fp.write('{}\n'.format(name))
+        # do a last yield to pass counts
+        yield dcnt, cnt, lcnt, None, None
+
+    with open(os.path.join(os.path.dirname(root), 'resources','icon_requirements.txt'), 'w') as fp:
+        for di, ci, lcnt, m,name in func():
+            if m and name:
+                print '{:<40s}{}'.format(name, m)
+                fp.write('{}\n'.format(name))
+
     print 'examined {} files from {} directories {}'.format(ci, di, lcnt)
 
 
