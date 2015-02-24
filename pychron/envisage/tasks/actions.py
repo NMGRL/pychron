9142f50f38588fd1e866696a9422018697097b1e
Jake Ross <jirhiker@gmail.com>
2015-02-24 16:45:59 -0700

    updater testing

--- pychron/envisage/tasks/actions.py
+++ pychron/envisage/tasks/actions.py
@@ -103,7 +103,7 @@ class DemoAction(Action):
 
     def perform(self, event):
         app = event.task.application
-        app.info('Demo message: {}'.format('Hello version 1.0'))
+        app.info('Demo message: {}'.format('Hello version 1.1'))
 
 
 class StartupTestsAction(Action):
