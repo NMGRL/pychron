6e9f9b7ef30c0ff8c377b5ec788cec134e82b702
Jake Ross <jirhiker@gmail.com>
2015-05-22 17:44:40 -0600

    updated peak center threshold

--- pychron/experiment/automated_run/automated_run.py
+++ pychron/experiment/automated_run/automated_run.py
@@ -29,7 +29,7 @@ from threading import Thread, Event as TEvent
 from uncertainties import ufloat, nominal_value, std_dev
 from numpy import Inf
 # ============= local library imports  ==========================
-from pychron.core.helpers.filetools import add_extension, get_path
+from pychron.core.helpers.filetools import get_path
 from pychron.core.helpers.filetools import add_extension
 # from pychron.core.codetools.memory_usage import mem_log
 
@@ -168,6 +168,10 @@ class AutomatedRun(Loggable):
 
     min_ms_pumptime = Int(60)
     overlap_evt = None
+
+    peak_center_threshold1 = Int(10)
+    peak_center_threshold2 = Int(1.25)
+    peak_center_threshold_window = Int(10)
 
     # ===============================================================================
     # pyscript interface
@@ -474,12 +478,22 @@ class AutomatedRun(Loggable):
 
         return ret
 
-    def py_peak_center(self, detector=None, save=True, **kw):
+    def py_peak_center(self, detector=None, save=True, isotope=None, **kw):
         if not self._alive:
             return
+
         ion = self.ion_optics_manager
 
         if ion is not None:
+            if self.arar_age:
+                iso = self.arar_age.get_isotope(isotope)
+                v = iso.get_intensity()
+                if v < self.peak_center_threshold1:
+                    self.debug('peak center: {}={}<{}'.format(isotope, self.peak_center_threshold1))
+                    xs = v.xs[-self.peak_center_threshol_window:]
+                    if xs.mean() < self.peak_center_threshold2:
+                        self.warning('Skipping peak center. intensities to small.')
+
             if not self.plot_panel:
                 p = self._new_plot_panel(self.plot_panel, stack_order='top_to_bottom')
                 self.plot_panel = p
