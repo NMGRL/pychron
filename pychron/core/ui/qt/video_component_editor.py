74ab1d7fa400335e88bbcc21c099480fae70f79f
jirhiker <jirhiker@gmail.com>
2015-11-24 08:19:07 -0700

    updated video component editor

--- pychron/core/ui/qt/video_component_editor.py
+++ pychron/core/ui/qt/video_component_editor.py
@@ -17,10 +17,9 @@
 # ============= enthought library imports =======================
 from traits.api import Any, Int, Str, Event
 # ============= standard library imports ========================
-from PySide.QtCore import QTimer
-from pychron.core.ui.qt.gui import invoke_in_main_thread
-from stage_component_editor import _LaserComponentEditor, LaserComponentEditor
+from PySide.QtCore import QTimer, SIGNAL
 # ============= local library imports  ==========================
+from stage_component_editor import _LaserComponentEditor, LaserComponentEditor
 
 
 class _VideoComponentEditor(_LaserComponentEditor):
@@ -39,7 +38,8 @@ class _VideoComponentEditor(_LaserComponentEditor):
         super(_VideoComponentEditor, self).init(parent)
 
         self.playTimer = QTimer(self.control)
-        self.playTimer.timeout.connect(self.update)
+        # self.playTimer.timeout.connect(self.update)
+        self.control.connect(self.playTimer, SIGNAL('timeout()'), self.update)
 
         if self.value.fps:
             self.playTimer.setInterval(1000 / float(self.value.fps))
@@ -47,22 +47,23 @@ class _VideoComponentEditor(_LaserComponentEditor):
         self.value.on_trait_change(self.stop, 'closed_event')
 
         self.value.on_trait_change(self._update_fps, 'fps')
-        self.sync_value('stop_timer', 'stop_timer', mode='both')
+        self.sync_value('stop_timer', 'stop_timer', mode='from')
 
     def _update_fps(self):
         if self.value.fps:
             self.playTimer.setInterval(1000 / float(self.value.fps))
 
     def stop(self):
+        print 'VideoComponentEditor stop'
         try:
             self.playTimer.stop()
         except RuntimeError:
-            del self.playTimer
+            pass
 
     def update(self):
         if self.control:
-            invoke_in_main_thread(self.value.request_redraw)
-            # self.value.request_redraw()
+            # invoke_in_main_thread(self.value.request_redraw)
+            self.value.request_redraw()
 
     def _stop_timer_fired(self):
         print 'VideoComponentEditor stopping playTimer'
