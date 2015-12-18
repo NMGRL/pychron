061385e382c6923c795f813b9f95f72dfadf5e34
Jake Ross <jirhiker@gmail.com>
2015-12-18 15:17:10 -0700

    started mv reorganization

--- pychron/mv/machine_vision_manager.py
+++ pychron/mv/machine_vision_manager.py
@@ -13,76 +13,25 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # ===============================================================================
-from traits.etsconfig.etsconfig import ETSConfig
-ETSConfig.toolkit = 'qt4'
-
 
 # ============= enthought library imports =======================
 from traits.api import Instance, Float, Any
-
 # ============= standard library imports ========================
 from threading import Timer
-from numpy import asarray
 # ============= local library imports  ==========================
-from pychron.managers.manager import Manager
+from pychron.loggable import Loggable
 from pychron.image.video import Video
-# from pychron.image.image import StandAloneImage
-from pychron.image.cv_wrapper import get_size, crop, grayspace
 from pychron.image.standalone_image import StandAloneImage
-# from pychron.image.cvwrapper import get_size, crop, grayspace
-
-from pychron.mv.co2_locator import CO2Locator
 
 
-class MachineVisionManager(Manager):
+class MachineVisionManager(Loggable):
     video = Instance(Video)
-#     target_image = Instance(StandAloneImage)
     pxpermm = Float(23)
-    laser_manager = Any
-
-    def new_co2_locator(self):
-        c = CO2Locator(pxpermm=self.pxpermm)
-        return c
 
     def new_image_frame(self):
         if self.video:
             src = self.video.get_frame()
             return src
-
-# ===============================================================================
-# image manipulation
-# ===============================================================================
-    def _crop_image(self, src, cw, ch):
-        CX, CY = 0, 0
-        cw_px = int(cw * self.pxpermm)
-        ch_px = int(ch * self.pxpermm)
-        w, h = get_size(src)
-
-        x = int((w - cw_px) / 2 + CX)
-        y = int((h - ch_px) / 2 + CY)
-
-#        print w / self.pxpermm, cw_px / self.pxpermm
-#        ra = 1
-#        print self.pxpermm * ra
-#        print w / float(cw)
-#        self.cpxpermm = w / float(cw) / 2.
-#        print h / float(ch), w / float(cw)
-#        print self.pxpermm * float(w) / cw_px
-#        self.cpxpermm = self.pxpermm * w / cw
-#        print self.cpxpermm, w / cw
-#        print w, cw_px
-#        print cw, w / (cw * self.pxpermm)
-#        self.croppixels = (cw_px, ch_px)
-#        self.croprect = (x, y, cw_px, ch_px)
-#        cw_px = ch_px = 107
-
-        r = 4 - cw_px % 4
-        cw_px = ch_px = cw_px + r
-
-        return asarray(crop(src, x, y, cw_px, ch_px))
-
-    def _gray_image(self, src):
-        return grayspace(src)
 
     def view_image(self, im, auto_close=True):
         # use a manager to open so will auto close on quit
@@ -94,25 +43,46 @@ class MachineVisionManager(Manager):
 
     def new_image(self, frame=None, title='AutoCenter',
                   view_id='target'):
+        im = StandAloneImage(title=title,
+                             id='pychron.machine_vision.{}'.format(view_id))
 
-#         if self.target_image is not None:
-#             self.target_image.close_ui()
-        im = StandAloneImage(
-                             title=title,
-                             id='pychron.machine_vision.{}'.format(view_id)
-
-                             )
-
-#         self.target_image = im
         if frame is not None:
             im.load(frame, swap_rb=True)
 
         return im
 
-#         from threading import Thread
-#         t = Thread(target=self._test)
-#         t.start()
-
-
-
 # ============= EOF =============================================
+# # ===============================================================================
+# # image manipulation
+# # ===============================================================================
+#     def _crop_image(self, src, cw, ch):
+#         CX, CY = 0, 0
+#         cw_px = int(cw * self.pxpermm)
+#         ch_px = int(ch * self.pxpermm)
+#         w, h = get_size(src)
+#
+#         x = int((w - cw_px) / 2 + CX)
+#         y = int((h - ch_px) / 2 + CY)
+#
+# #        print w / self.pxpermm, cw_px / self.pxpermm
+# #        ra = 1
+# #        print self.pxpermm * ra
+# #        print w / float(cw)
+# #        self.cpxpermm = w / float(cw) / 2.
+# #        print h / float(ch), w / float(cw)
+# #        print self.pxpermm * float(w) / cw_px
+# #        self.cpxpermm = self.pxpermm * w / cw
+# #        print self.cpxpermm, w / cw
+# #        print w, cw_px
+# #        print cw, w / (cw * self.pxpermm)
+# #        self.croppixels = (cw_px, ch_px)
+# #        self.croprect = (x, y, cw_px, ch_px)
+# #        cw_px = ch_px = 107
+#
+#         r = 4 - cw_px % 4
+#         cw_px = ch_px = cw_px + r
+#
+#         return asarray(crop(src, x, y, cw_px, ch_px))
+#
+#     def _gray_image(self, src):
+#         return grayspace(src)
--- pychron/mv/test_mv_manager.py
+++ pychron/mv/test_mv_manager.py
@@ -17,177 +17,195 @@
 # ============= enthought library imports =======================
 import time
 
-from traits.api import Button, Instance
-from traitsui.api import View, Item
+from traits.api import Button, Instance, HasTraits
+from traitsui.api import View, Item, UItem
 
-from pychron.mv.machine_vision_manager import MachineVisionManager
-from pychron.mv.mv_image import MVImage
+# from pychron.mv.machine_vision_manager import MachineVisionManager
+# from pychron.mv.mv_image import MVImage
 
 
 # ============= standard library imports ========================
 # ============= local library imports  ==========================
+from pychron.mv.autocenter_manager import AutoCenterManager
 
-class TestMVManager(MachineVisionManager):
-    step = Button
-    test_image = Instance(MVImage, ())
 
-    def _step_fired(self):
-        self.step_signal.set()
+class TestAutocenter(HasTraits):
+    test1_button = Button('Test1')
 
-    def traits_view(self):
-        return View(Item('test'),
-                    Item('step'),
-                    Item('test_image', show_label=False,
-                         style='custom'),
-                    resizable=True
-                    )
+    def _test1(self):
+        print 'test1'
+        a = AutoCenterManager()
+        new = a.calculate_new_center(0,0)
+        print 'new center {}'.format(new)
 
-    def _test_fired(self):
-        from pychron.globals import globalv
-
-        p = '/Users/ross/Sandbox/test_target.jpg'
-#         p = '/Users/ross/Sandbox/pos_err/pos_err_200_0-002.jpg'
-        p = '/Users/ross/Sandbox/poserror/pos_err_221_0-007.jpg'
-#         p = '/Users/ross/Sandbox/poserror/snapshot009.jpg'
-        # force video to reload test image
-        self.video.source_frame = None
-        globalv.video_test_path = p
-
-        im = self.setup_image()
-
-#         self._test2(im)
-        from pychron.core.ui.thread import Thread
-        t = Thread(target=self._test2, args=(im,))
-        t.start()
-        self._t = t
-
-# ===============================================================================
-# tests
-# ===============================================================================
-    def _test(self, im):
-
-        paths = (
-#                 ('/Users/ross/Sandbox/pos_err/snapshot007.jpg', 1.25),
-#                 ('/Users/ross/Sandbox/pos_err/pos_err_221_0-005.jpg', 1.25),
-#                 ('/Users/ross/Sandbox/pos_err/pos_err_207_0-002.jpg', 1.25),
-#                 ('/Users/ross/Sandbox/pos_err/pos_err_209_0-001.jpg', 1.25),
-#                 ('/Users/ross/Sandbox/pos_err/pos_err_210_0-001.jpg', 1.25),
-#                 ('/Users/ross/Sandbox/pos_err/pos_err_220_0-001.jpg', 1.25),
-#                 ('/Users/ross/Sandbox/pos_err/pos_err_221_0-001.jpg', 1.25),
-#                 ('/Users/ross/Sandbox/pos_err/pos_err_221_0-002.jpg', 1.25),
-#                 ('/Users/ross/Sandbox/pos_err/pos_err_221_0-003.jpg', 1.25),
-#                 ('/Users/ross/Sandbox/pos_err/pos_err_221_0-004.jpg', 1.25),
-#                 ('/Users/ross/Sandbox/pos_err/pos_err_200_0-001.jpg', 1.25),
-#                 ('/Users/ross/Sandbox/pos_err/pos_err_200_0-002.jpg', 1.25),
-#                 ('/Users/ross/Sandbox/pos_err/pos_err_201_0-001.jpg', 1.25),
-#                 ('/Users/ross/Sandbox/pos_err/pos_err_202_0-001.jpg', 1.25),
-#                 ('/Users/ross/Sandbox/pos_err/pos_err_203_0-001.jpg', 1.25),
-#                 ('/Users/ross/Sandbox/pos_err/pos_err_204_0-001.jpg', 1.25),
-                 ('/Users/ross/Sandbox/pos_err/pos_err_206_0-001.jpg', 1.25),
-#                  ('/Users/ross/Sandbox/pos_err/pos_err_206_1-001.jpg', 1.25),
-#                  ('/Users/ross/Sandbox/pos_err/pos_err_207_0-001.jpg', 1.25),
-#                  ('/Users/ross/Sandbox/pos_err/pos_err_52001.jpg', 2.25),
-#                 ('/Users/ross/Sandbox/pos_err/pos_err_52001.tiff', 2.25),
-#                  ('/Users/ross/Sandbox/pos_err/pos_err_52002.jpg', 2.25),
-#                 ('/Users/ross/Sandbox/pos_err/pos_err_53001.jpg', 2.25),
-#                 ('/Users/ross/Sandbox/pos_err/pos_err_53002.jpg', 2.25),
-#                 ('/Users/ross/Sandbox/pos_err/pos_err_53003.jpg', 2.25),
-#                 ('/Users/ross/Sandbox/pos_err/pos_err_54001.jpg', 2.25),
-                )
-        fails = 0
-        times = []
-
-#         im = self.target_image
-        for p, dim in paths[:]:
-            from pychron.globals import globalv
-            # force video to reload test image
-            self.video.source_frame = None
-            globalv.video_test_path = p
-#             return
-#             im.source_frame = self.new_image_frame()
-            frm = self.new_image_frame()
-#             self.target_image.load()
-
-            cw = ch = dim * 3.2
-#            cw = ch = dim
-            frame = self._crop_image(frm, cw, ch)
-            im.source_frame = frame
-#            time.sleep(1)
-#            continue
-#            self.target_image.set_frame(0, frame)
-
-#            loc.pxpermm = self.cpxpermm
-
-#            loc.croppixels = (cw * self.pxpermm, ch * self.pxpermm)
-
-            loc = self.new_co2_locator()
-
-            st = time.time()
-            dx, dy = loc.find(im, frame, dim * self.pxpermm)
-
-            times.append(time.time() - st)
-            if dx and dy:
-                self.info('SUCCESS path={}'.format(p))
-                self.info('calculated deviation {:0.3f},{:0.3f}'.format(dx, dy))
-            else:
-                fails += 1
-                self.info('FAIL    path={}'.format(p))
-            time.sleep(1)
-
-        if times:
-            n = len(paths)
-            self.info('failed to find center {}/{} times'.format(fails, n))
-            self.info('execution times: min={} max={} avg={}'.format(min(times), max(times), sum(times) / n))
-
-#        def foo():
-#            from pylab import show, plot
-#            plot(times)
-#            show()
-#        do_later(foo)
-
-    def _test2(self, im):
-
-        dim = 1.0
-
-        frame = self.new_image_frame()
-
-        cw = ch = dim * 3.2
-
-        frame = self._crop_image(frame, cw, ch)
-#         print frame
-#         im.source_frame = frame
-        loc = self.new_co2_locator()
-        from threading import Event
-        evt = Event()
-        self.step_signal = evt
-        loc.step_signal = evt
-        loc.test_image = self.test_image
-
-        dx, dy = loc.find(im, frame, dim * self.pxpermm)
-#         print dx, dy
-    def setup_image(self):
-        frame = self.new_image_frame()
-        im = self.new_image(frame)
-        self.view_image(im)
-        return im
-
-def test1():
-    from pychron.image.video import Video
-    from pychron.globals import globalv
-    globalv.video_test = True
-    globalv.video_test_path = '/Users/ross/Sandbox/pos_err/snapshot007.jpg'
-#    globalv.video_test_path = '/Users/ross/Sandbox/pos_err/pos_err_53002.jpg'
-    globalv.video_test_path = '/Users/ross/Sandbox/pos_err/pos_err_221_0-005.jpg'
-
-#    globalv.video_test_path = '/Users/ross/Sandbox/pos_err/diodefailsnapshot.jpg'
-    video = Video()
-    video.open()
-    mv = MachineVisionManager(video=video)
-    mv.configure_traits()
-
+    def _test1_button_fired(self):
+        self._test1()
 
 if __name__ == '__main__':
-    from pychron.core.helpers.logger_setup import logging_setup
-    logging_setup('mv')
-    test()
+    t = TestAutocenter()
+    t.configure_traits(view=View(UItem('test1_button')))
 # ============= EOF =============================================
+
+# class TestMVManager(MachineVisionManager):
+#     step = Button
+#     test_image = Instance(MVImage, ())
+#
+#     def _step_fired(self):
+#         self.step_signal.set()
+#
+#     def traits_view(self):
+#         return View(Item('test'),
+#                     Item('step'),
+#                     Item('test_image', show_label=False,
+#                          style='custom'),
+#                     resizable=True
+#                     )
+#
+#     def _test_fired(self):
+#         from pychron.globals import globalv
+#
+#         p = '/Users/ross/Sandbox/test_target.jpg'
+# #         p = '/Users/ross/Sandbox/pos_err/pos_err_200_0-002.jpg'
+#         p = '/Users/ross/Sandbox/poserror/pos_err_221_0-007.jpg'
+# #         p = '/Users/ross/Sandbox/poserror/snapshot009.jpg'
+#         # force video to reload test image
+#         self.video.source_frame = None
+#         globalv.video_test_path = p
+#
+#         im = self.setup_image()
+#
+# #         self._test2(im)
+#         from pychron.core.ui.thread import Thread
+#         t = Thread(target=self._test2, args=(im,))
+#         t.start()
+#         self._t = t
+#
+# # ===============================================================================
+# # tests
+# # ===============================================================================
+#     def _test(self, im):
+#
+#         paths = (
+# #                 ('/Users/ross/Sandbox/pos_err/snapshot007.jpg', 1.25),
+# #                 ('/Users/ross/Sandbox/pos_err/pos_err_221_0-005.jpg', 1.25),
+# #                 ('/Users/ross/Sandbox/pos_err/pos_err_207_0-002.jpg', 1.25),
+# #                 ('/Users/ross/Sandbox/pos_err/pos_err_209_0-001.jpg', 1.25),
+# #                 ('/Users/ross/Sandbox/pos_err/pos_err_210_0-001.jpg', 1.25),
+# #                 ('/Users/ross/Sandbox/pos_err/pos_err_220_0-001.jpg', 1.25),
+# #                 ('/Users/ross/Sandbox/pos_err/pos_err_221_0-001.jpg', 1.25),
+# #                 ('/Users/ross/Sandbox/pos_err/pos_err_221_0-002.jpg', 1.25),
+# #                 ('/Users/ross/Sandbox/pos_err/pos_err_221_0-003.jpg', 1.25),
+# #                 ('/Users/ross/Sandbox/pos_err/pos_err_221_0-004.jpg', 1.25),
+# #                 ('/Users/ross/Sandbox/pos_err/pos_err_200_0-001.jpg', 1.25),
+# #                 ('/Users/ross/Sandbox/pos_err/pos_err_200_0-002.jpg', 1.25),
+# #                 ('/Users/ross/Sandbox/pos_err/pos_err_201_0-001.jpg', 1.25),
+# #                 ('/Users/ross/Sandbox/pos_err/pos_err_202_0-001.jpg', 1.25),
+# #                 ('/Users/ross/Sandbox/pos_err/pos_err_203_0-001.jpg', 1.25),
+# #                 ('/Users/ross/Sandbox/pos_err/pos_err_204_0-001.jpg', 1.25),
+#                  ('/Users/ross/Sandbox/pos_err/pos_err_206_0-001.jpg', 1.25),
+# #                  ('/Users/ross/Sandbox/pos_err/pos_err_206_1-001.jpg', 1.25),
+# #                  ('/Users/ross/Sandbox/pos_err/pos_err_207_0-001.jpg', 1.25),
+# #                  ('/Users/ross/Sandbox/pos_err/pos_err_52001.jpg', 2.25),
+# #                 ('/Users/ross/Sandbox/pos_err/pos_err_52001.tiff', 2.25),
+# #                  ('/Users/ross/Sandbox/pos_err/pos_err_52002.jpg', 2.25),
+# #                 ('/Users/ross/Sandbox/pos_err/pos_err_53001.jpg', 2.25),
+# #                 ('/Users/ross/Sandbox/pos_err/pos_err_53002.jpg', 2.25),
+# #                 ('/Users/ross/Sandbox/pos_err/pos_err_53003.jpg', 2.25),
+# #                 ('/Users/ross/Sandbox/pos_err/pos_err_54001.jpg', 2.25),
+#                 )
+#         fails = 0
+#         times = []
+#
+# #         im = self.target_image
+#         for p, dim in paths[:]:
+#             from pychron.globals import globalv
+#             # force video to reload test image
+#             self.video.source_frame = None
+#             globalv.video_test_path = p
+# #             return
+# #             im.source_frame = self.new_image_frame()
+#             frm = self.new_image_frame()
+# #             self.target_image.load()
+#
+#             cw = ch = dim * 3.2
+# #            cw = ch = dim
+#             frame = self._crop_image(frm, cw, ch)
+#             im.source_frame = frame
+# #            time.sleep(1)
+# #            continue
+# #            self.target_image.set_frame(0, frame)
+#
+# #            loc.pxpermm = self.cpxpermm
+#
+# #            loc.croppixels = (cw * self.pxpermm, ch * self.pxpermm)
+#
+#             loc = self.new_co2_locator()
+#
+#             st = time.time()
+#             dx, dy = loc.find(im, frame, dim * self.pxpermm)
+#
+#             times.append(time.time() - st)
+#             if dx and dy:
+#                 self.info('SUCCESS path={}'.format(p))
+#                 self.info('calculated deviation {:0.3f},{:0.3f}'.format(dx, dy))
+#             else:
+#                 fails += 1
+#                 self.info('FAIL    path={}'.format(p))
+#             time.sleep(1)
+#
+#         if times:
+#             n = len(paths)
+#             self.info('failed to find center {}/{} times'.format(fails, n))
+#             self.info('execution times: min={} max={} avg={}'.format(min(times), max(times), sum(times) / n))
+#
+# #        def foo():
+# #            from pylab import show, plot
+# #            plot(times)
+# #            show()
+# #        do_later(foo)
+#
+#     def _test2(self, im):
+#
+#         dim = 1.0
+#
+#         frame = self.new_image_frame()
+#
+#         cw = ch = dim * 3.2
+#
+#         frame = self._crop_image(frame, cw, ch)
+# #         print frame
+# #         im.source_frame = frame
+#         loc = self.new_co2_locator()
+#         from threading import Event
+#         evt = Event()
+#         self.step_signal = evt
+#         loc.step_signal = evt
+#         loc.test_image = self.test_image
+#
+#         dx, dy = loc.find(im, frame, dim * self.pxpermm)
+# #         print dx, dy
+#     def setup_image(self):
+#         frame = self.new_image_frame()
+#         im = self.new_image(frame)
+#         self.view_image(im)
+#         return im
+#
+# def test1():
+#     from pychron.image.video import Video
+#     from pychron.globals import globalv
+#     globalv.video_test = True
+#     globalv.video_test_path = '/Users/ross/Sandbox/pos_err/snapshot007.jpg'
+# #    globalv.video_test_path = '/Users/ross/Sandbox/pos_err/pos_err_53002.jpg'
+#     globalv.video_test_path = '/Users/ross/Sandbox/pos_err/pos_err_221_0-005.jpg'
+#
+# #    globalv.video_test_path = '/Users/ross/Sandbox/pos_err/diodefailsnapshot.jpg'
+#     video = Video()
+#     video.open()
+#     mv = MachineVisionManager(video=video)
+#     mv.configure_traits()
+#
+#
+# if __name__ == '__main__':
+#     from pychron.core.helpers.logger_setup import logging_setup
+#     logging_setup('mv')
+#     test()
