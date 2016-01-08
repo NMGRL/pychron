4884fb6bfd0e318a2a306ab6587d926830ded4e9
Jake Ross <jirhiker@gmail.com>
2016-01-08 16:17:42 -0700

    updated install script

--- app_utils/install2.sh
+++ app_utils/install2.sh
@@ -16,17 +16,20 @@ CONDA_ENV=pychron
 PYCHRONDATA_PREFIX=~/Pychron
 DOWNLOAD_URL=https://github.com/NMGRL/pychron/archive/v3.tar.gz
 MINICONDA_URL=https://repo.continuum.io/miniconda/Miniconda-latest-MacOSX-x86_64.sh
+GITLFS_URL=https://github.com/github/git-lfs/releases/download/v1.1.0/git-lfs-darwin-amd64-1.1.0.tar.gz
 MINICONDA_PREFIX=$HOME/miniconda2
 MINICONDA_INSTALLER_SCRIPT=miniconda_installer.sh
 LAUNCHER_SCRIPT_PATH=pychron_launcher.sh
+ENTHOUGHT_DIR=$HOME/.enthought
 
 CONDA_REQ="statsmodels
-PyYAML>=3
-traits>=4.4
-traitsui>=4.4
-chaco>=4.4
-enable>=4.3
-pyface>=4.4
+scikit-learn
+PyYAML
+traits
+traitsui
+chaco>
+enable
+pyface
 envisage
 sqlalchemy
 Reportlab
@@ -67,9 +70,9 @@ INITIALIZATION="<root>\n
 </root>\n
 "
 
-DVC_PREFS="[pychron.dvc]
-organization=NMGRL
-meta_repo_name=meta
+DVC_PREFS="[pychron.dvc]\n
+organization=NMGRL\n
+meta_repo_name=meta\n
 "
 
 # =========== Setup Working dir ===========
@@ -128,10 +131,22 @@ else
     mkdir ${PYCHRONDATA_PREFIX}/setupfiles
     mkdir ${PYCHRONDATA_PREFIX}/preferences
 
-    echo ${DVC_PREFS} > ${PYCHRONDATA_PREFIX}/preferences/dvc.ini
-    echo ${INITIALIZATION} > ${PYCHRONDATA_PREFIX}/setupfiles/initialization.xml
+    printf ${DVC_PREFS} > ${PYCHRONDATA_PREFIX}/preferences/dvc.ini
+    printf ${INITIALIZATION} > ${PYCHRONDATA_PREFIX}/setupfiles/initialization
+    .xml
 fi
 
+# =========== Git LFS ===============
+
+if type git lfs >/dev/null
+then
+    echo Git-LFS already installed
+else
+    echo Installing Git-LFS
+    curl -L ${GITLFS_URL} -o git-lfs.tar.gz
+    tar -xf ./git-lfs.tar.gz -C ./git-lfs --strip-components=1
+    ./git-lfs/install.sh
+fi
 
 # =========== Unpack Release ===============
 cd ${PYCHRONDATA_PREFIX}
@@ -141,11 +156,21 @@ tar -xf ./pychron_src.tar.gz -C ./src --strip-components=1
 
 # ========== Launcher Script ===============
 touch "${LAUNCHER_SCRIPT_PATH}"
-echo ROOT=${PYCHRONDATA_PREFIX}/src > "${LAUNCHER_SCRIPT_PATH}"
+printf ROOT=${PYCHRONDATA_PREFIX}/src > "${LAUNCHER_SCRIPT_PATH}"
 
-echo ENTRY_POINT=\$ROOT/launchers/pyexperiment_debug.py >> "${LAUNCHER_SCRIPT_PATH}"
-echo export PYTHONPATH=\$ROOT >> "${LAUNCHER_SCRIPT_PATH}"
+printf ENTRY_POINT=\$ROOT/launchers/pyexperiment_debug.py >> "${LAUNCHER_SCRIPT_PATH}"
+printf export PYTHONPATH=\$ROOT >> "${LAUNCHER_SCRIPT_PATH}"
 
-echo ${MINICONDA_PREFIX}/envs/${CONDA_ENV}/bin/python \$ENTRY_POINT >> "${LAUNCHER_SCRIPT_PATH}"
+printf ${MINICONDA_PREFIX}/envs/${CONDA_ENV}/bin/python \$ENTRY_POINT >> "${LAUNCHER_SCRIPT_PATH}"
 chmod +x ${LAUNCHER_SCRIPT_PATH}
 cp ${LAUNCHER_SCRIPT_PATH} ~/Desktop/
+
+# ========= Enthought directory ============
+if [ -d ${ENTHOUGHT_DIR} ]
+then
+    echo ${ENTHOUGHT_DIR} already exists
+else
+    echo Making root directory ${ENTHOUGHT_DIR}
+    mkdir ${ENTHOUGHT_DIR}
+fi
+
