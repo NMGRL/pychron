757a1e5f9b452e6121cf419cdc989fc8bbd7c209
obama <jirhiker@gmail.com>
2016-01-28 14:26:58 -0700

    set run to truncated if user truncated

--- pychron/experiment/automated_run/data_collector.py
+++ pychron/experiment/automated_run/data_collector.py
@@ -340,6 +340,13 @@ class DataCollector(Consoleable):
         # self.debug('user_counts={}, script_counts={}, original_counts={}'.format(user_counts,
         #                                                                          script_counts,
         #                                                                          original_counts))
+
+        def set_truncated():
+            self.state = 'truncated'
+            self.automated_run.truncated = True
+            self.automated_run.state = 'truncated'
+            return 'break'
+
         if not self._alive:
             self.info('measurement iteration executed {}/{} counts'.format(*count_args))
             return 'cancel'
@@ -348,33 +355,31 @@ class DataCollector(Consoleable):
             if i > user_counts:
                 self.info('user termination. measurement iteration executed {}/{} counts'.format(*count_args))
                 self.plot_panel.total_counts -= (original_counts - i)
-                return 'break'
+                return set_truncated()
+
         elif script_counts != original_counts:
-            print script_counts, original_counts, i
             if i > script_counts:
                 self.info('script termination. measurement iteration executed {}/{} counts'.format(*count_args))
-                return 'break'
+                return set_truncated()
+
         elif i > original_counts:
             return 'break'
 
         if self._truncate_signal:
             self.info('measurement iteration executed {}/{} counts'.format(*count_args))
             self._truncate_signal = False
-
-            return 'break'
+            return set_truncated()
 
         if self.check_conditionals:
             truncation_conditional = self._check_conditionals(self.truncation_conditionals, i)
             if truncation_conditional:
                 self.info('truncation conditional {}. measurement iteration executed {}/{} counts'.format(
                         truncation_conditional.message, j, original_counts), color='red')
-                self.state = 'truncated'
-                self.automated_run.truncated = True
-                self.automated_run.state = 'truncated'
+
                 self.measurement_script.abbreviated_count_ratio = truncation_conditional.abbreviated_count_ratio
                 self.automated_run.show_conditionals(tripped=truncation_conditional)
                 #                self.condition_truncated = True
-                return 'break'
+                return set_truncated()
 
             action_conditional = self._check_conditionals(self.action_conditionals, i)
             if action_conditional:
