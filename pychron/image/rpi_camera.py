30f41ba1e30bf8002fdb343a4dae8f983eaaa10c
Jake Ross <jirhiker@gmail.com>
2016-03-01 09:09:52 -0700

    updated rpi camera configuration

--- pychron/image/rpi_camera.py
+++ pychron/image/rpi_camera.py
@@ -24,14 +24,64 @@ from pychron.paths import paths
 
 
 class RPiCamera(HeadlessConfigLoadable):
+
+    sharpness = 0
+    contrast = 0
+    brightness = 50
+    saturation = 0
+    ISO = 0
+    video_stabilization = False
+    exposure_compensation = 0
+
+    # exposure modes
+    # off, auto, night, nightpreview, backlight, spotlight, sports, snow, beach,
+    # verylong, fixedfps, antishake, fireworks,
+    exposure_mode = 'auto'
+
+    meter_mode = 'average'  # stop, average, backlit, matrix
+
+    # awb_modes
+    # off, auto, sunlight, cloudy, shade, tungsten, fluorescent, incandescent, flash, horizon
+    awb_mode = 'auto'
+
+    # image effects
+    # none, negative, solarize, sketch, denoise, emboss, oilpaint, hatch,
+    # gpen, pastel, watercolor,film, blur, saturation, colorswap, washedout,
+    # posterise, colorpoint, colorbalance, cartoon, deinterlace1, deinterlace2
+    image_effect = 'none'
+
+    color_effects = None # (u,v)
+    rotation = 0  # 0,90,180,270
     hflip = False
     vflip = False
+    crop = (0.0, 0.0, 1.0, 1.0)
 
     def load_additional_args(self, *args, **kw):
         config = self.get_configuration()
 
         self.set_attribute(config, 'hflip', 'General', 'hflip', cast='boolean')
         self.set_attribute(config, 'vflip', 'General', 'vflip', cast='boolean')
+
+        self.set_attribute(config, 'sharpness', 'Settings', 'sharpness', cast='int')
+        self.set_attribute(config, 'contrast', 'Settings', 'contrast', cast='int')
+        self.set_attribute(config, 'brightness', 'Settings', 'brightness', cast='int')
+        self.set_attribute(config, 'saturation', 'Settings', 'saturation', cast='int')
+        self.set_attribute(config, 'ISO', 'Settings', 'ISO', cast='int')
+        self.set_attribute(config, 'video_stabilization', 'Settings', 'video_stabilization', cast='boolean')
+        self.set_attribute(config, 'exposure_compensation', 'Settings', 'exposure_compensation', cast='int')
+        self.set_attribute(config, 'exposure_mode', 'Settings', 'exposure_mode')
+        self.set_attribute(config, 'meter_mode', 'Settings', 'meter_mode')
+        self.set_attribute(config, 'awb_mode', 'Settings', 'awb_mode')
+        self.set_attribute(config, 'image_effect', 'Settings', 'image_effect')
+        self.set_attribute(config, 'color_effects', 'Settings', 'color_effects')
+        self.set_attribute(config, 'rotation', 'Settings', 'rotation', cast='int')
+        self.set_attribute(config, 'hflip', 'Settings', 'hflip', cast='boolean')
+        self.set_attribute(config, 'vflip', 'Settings', 'vflip', cast='boolean')
+
+        crop = self.config_get(config, 'Settings', 'crop')
+        if crop:
+            self.crop = tuple(map(float, crop.split(',')))
+
         return True
 
     def get_image_array(self):
@@ -54,7 +104,11 @@ class RPiCamera(HeadlessConfigLoadable):
 
     # private
     def _setup_camera(self, camera):
-        camera.hflip = self.hflip
-        camera.vflip = self.vflip
+        attrs = ('sharpness', 'contrast', 'brightness', 'saturation', 'ISO',
+                 'video_stabilization', 'exposure_compensation', 'exposure_mode',
+                 'meter_mode', 'awb_mode', 'image_effect', 'color_effects',
+                 'rotation', 'hflip', 'vflip', 'crop')
+        for attr in attrs:
+            setattr(camera, attr, getattr(self, attr))
 
 # ============= EOF =============================================
