a7c436564288a9f7d511e4b0bfa8317e18e7f590
Jake Ross <jirhiker@gmail.com>
2016-03-04 13:36:09 -0700

    fixed check error

--- pychron/hardware/mdrive/base.py
+++ pychron/hardware/mdrive/base.py
@@ -290,7 +290,7 @@ class BaseMDrive(BaseLinearDrive):
     def _check_error(self):
         eflag = self._get_var('EF')
         if eflag == 1:
-            ecode = str(self._get_var('ER', as_int=False))
+            ecode = str(self._get_var('ER', as_int=False)).strip()
             estr = ERROR_MAP.get(ecode, 'See MCode Programming Manual')
             return ecode, estr
 
