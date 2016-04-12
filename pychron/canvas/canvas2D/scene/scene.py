c1e682d02606a65b67e715f485b14046d1b9eb27
Jake Ross <jirhiker@gmail.com>
2016-04-12 01:49:31 -0600

    added parametric translation

--- pychron/canvas/canvas2D/scene/scene.py
+++ pychron/canvas/canvas2D/scene/scene.py
@@ -210,6 +210,36 @@ class Scene(HasTraits):
     def _get_floats(self, elem, name):
         return map(float, elem.find(name).text.split(','))
 
+    def _get_translation(self, cp, elem, name='translation'):
+        x, y = elem.find(name).text.split(',')
+        try:
+            x = float(x)
+        except ValueError:
+            x = self._get_parameteric_translation(cp, x)
+
+        try:
+            y = float(y)
+        except ValueError:
+            y = self._get_parameteric_translation(cp, y)
+
+        return x, y
+
+    def _get_parameteric_translation(self, cp, tag):
+        v = 0
+        offset = 0
+        if '+' in tag:
+            tag, offset = tag.split('+')
+        elif '-' in tag:
+            tag, offset = tag.split('-')
+
+        offset = int(offset)
+        for p in cp.get_elements('param'):
+            if p.text.strip() == tag:
+                e = p.find('value')
+                v = e.text.strip()
+
+        return float(v) + offset
+
     def _make_color(self, c):
         if not isinstance(c, str):
             c = ','.join(map(str, map(int, c)))
