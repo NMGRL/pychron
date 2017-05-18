441293ce5055387b730235ab1b256f747e2a1ab2
Jake Ross <jirhiker@gmail.com>
2017-05-18 16:58:58 -0600

    addressed issue #1093

--- pychron/pipeline/plot/panels/figure_panel.py
+++ pychron/pipeline/plot/panels/figure_panel.py
@@ -143,17 +143,20 @@ class FigurePanel(HasTraits):
 
             for i, p in enumerate(plots):
                 g.plots[i].value_scale = p.scale
+                print 'seting ylimits', i
 
-                if p.has_ylimits():
-                    g.set_y_limits(p.ylimits[0], p.ylimits[1], plotid=i)
-                elif p.calculated_ymin or p.calculated_ymax:
-                    g.set_y_limits(p.calculated_ymin, p.calculated_ymax, plotid=i)
-                elif p.ymin or p.ymax:
+                if p.ymin or p.ymax:
+                    print 'has ymin max set', p.ymin, p.ymax
                     ymi, yma = p.ymin, p.ymax
                     if p.ymin > p.ymax:
                         yma = None
-
                     g.set_y_limits(ymi, yma, plotid=i)
+                elif p.has_ylimits():
+                    print 'has ylimits', p.ylimits[0], p.ylimits[1]
+                    g.set_y_limits(p.ylimits[0], p.ylimits[1], plotid=i)
+                elif p.calculated_ymin or p.calculated_ymax:
+                    print 'has calculated', p.calculated_ymin, p.calculated_ymax
+                    g.set_y_limits(p.calculated_ymin, p.calculated_ymax, plotid=i)
 
             if mi is None and ma is None:
                 mi, ma = 0, 100
