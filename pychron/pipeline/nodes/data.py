b819e42c7f83723b1a7f2f744e44bdf3fee58369
Jake Ross <jirhiker@gmail.com>
2017-05-19 13:37:14 -0600

    addressed issue #1098

--- pychron/pipeline/nodes/data.py
+++ pychron/pipeline/nodes/data.py
@@ -39,6 +39,7 @@ class DVCNode(BaseNode):
         from pychron.envisage.browser.view import BrowserView
 
         self.browser_model.activated()
+        self.browser_model.do_filter()
 
         if irradiation:
             self.browser_model.irradiation_enabled = True
