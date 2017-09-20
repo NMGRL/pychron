3621ea9e0e4ec6e5a8fe2154ca66072bb44cd2d0
Jake Ross <jirhiker@gmail.com>
2017-09-20 16:07:17 -0600

    updated camera

--- pychron/image/camera.py
+++ pychron/image/camera.py
@@ -15,11 +15,7 @@
 # ===============================================================================
 
 # ============= enthought library imports =======================
-import os
-
-from traits.api import HasTraits, provides, Button, Event, Range,Any
-# ============= standard library imports ========================
-# ============= local library imports  ==========================
+from traits.api import HasTraits, provides, Button, Event, Range, Any, Bool, TraitError
 from traits.has_traits import on_trait_change
 from traitsui.api import View, UItem, VGroup, VFold, HSplit, Item, HGroup, spring
 from traitsui.menu import Action, ToolBar
@@ -29,40 +25,43 @@ from pychron.core.ui.qt.camera_editor import CameraEditor
 from pychron.envisage.resources import icon
 from pychron.image.i_camera import ICamera
 from pychron.image.toupcam.camera import ToupCamCamera
-from pychron.paths import paths
-from pychron.persistence_loggable import PersistenceMixin
+
+
+# ============= standard library imports ========================
+# ============= local library imports  ==========================
 
 
 @provides(ICamera)
-class Camera(HasTraits, PersistenceMixin):
+class Camera(HasTraits):
     _device = Any
     configure = Button
     snapshot_event = Event
-    temperature = Range(2000, 15000, mode='slider', dump=True)
-    tint = Range(200, 2500, mode='slider', dump=True)
-    hue = Range(-180, 180, mode='slider', dump=True)
+    temperature = Range(2000, 15000, mode='slider')
+    tint = Range(200, 2500, mode='slider')
+    hue = Range(-180, 180, mode='slider')
     saturation = Range(0, 255, mode='slider')
     brightness = Range(-64, 64, mode='slider')
     contrast = Range(-100, 100, mode='slider')
     gamma = Range(0, 180, mode='slider')
 
+    auto_exposure_enabled = Bool
+    exposure_time = Range(0, 100, mode='slider')
     awb_button = Button('Auto')
     contrast_default_button = Button('Defaults')
     hue_default_button = Button('Defaults')
 
     _no_update = False
 
-    @property
-    def persistence_path(self):
-        try:
-            return os.path.join(paths.hidden_dir, 'camera_settings')
-        except AttributeError:
-            return os.path.join(os.path.expanduser('~'), 'Desktop', 'camera_settings')
+    # @property
+    # def persistence_path(self):
+    #     try:
+    #         return os.path.join(paths.hidden_dir, 'camera_settings')
+    #     except AttributeError:
+    #         return os.path.join(os.path.expanduser('~'), 'Desktop', 'camera_settings')
 
     def activate(self):
         self.open()
-        self.load()
-        self._update_color()
+        self._update_parameters()
 
     def save_jpeg(self, p):
         self._device.save_jpeg(p)
@@ -78,7 +77,7 @@ class Camera(HasTraits, PersistenceMixin):
     def _contrast_default_button_fired(self):
         self.trait_set(contrast=0, gamma=100)
 
-    @on_trait_change('hue,saturation,brightness,contrast,gamma')
+    @on_trait_change('hue,saturation,brightness,contrast,gamma,auto_exposure, exposure_time')
     def _handle_color_change(self, name, new):
         if self._device is not None:
             if not self._no_update:
@@ -103,15 +102,16 @@ class Camera(HasTraits, PersistenceMixin):
         if not self._no_update:
             self._device.set_temperature_tint(self.temperature, self.tint)
 
-    def _update_color(self):
+    def _update_parameters(self):
         self._update_temptint()
         with no_update(self):
             d = {k: getattr(self._device, 'get_{}'.format(k))() for k in
-                 ('hue', 'saturation', 'brightness', 'contrast', 'gamma')}
-            print 'dasfa', d
+                 ('hue', 'saturation', 'brightness', 'contrast', 'gamma',
+                  'auto_exposure', 'exposure_time')}
+
             try:
                 self.trait_set(**d)
-            except TypeError:
+            except TraitError:
                 pass
 
     #
@@ -179,12 +179,15 @@ class Camera(HasTraits, PersistenceMixin):
         #                   Item('tint'),
         #                   hue_grp, c_gamma_grp)
         #
-        # exposure_grp = VGroup(label='Exposure')
+        exposure_grp = VGroup(Item('auto_exposure_enabled'),
+                              Item('exposure_time', enabled_when='not auto_exposure_enabled'),
+                              label='Exposure')
         white_balance_grp = VGroup(UItem('awb_button'),
                                    label='White Balance')
         # color_grp = VGroup(label='Color')
 
         ctrlgrp = VFold(hue_grp,
+                        exposure_grp,
                         c_gamma_grp,
                         white_balance_grp)
 
@@ -198,7 +201,8 @@ class Camera(HasTraits, PersistenceMixin):
                                  # Action(action='save_settings',
                                  #        image=icon('cog'))
                                  ),
-                 title='Camera')
+                 title='Camera',
+                 resizable=True)
         return v
 
         # _owners = None
@@ -267,7 +271,7 @@ class Camera(HasTraits, PersistenceMixin):
 
 if __name__ == '__main__':
     c = Camera()
-    c.load()
+    c.activate()
     c.configure_traits()
-    c.dump()
+    # c.dump()
 # ============= EOF =============================================
