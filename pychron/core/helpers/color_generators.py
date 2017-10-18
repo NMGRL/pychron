9fbb5a59a8e769445005c524d969c5f69c7102ab
felix <nmgrluser@gmail.com>
2017-10-18 15:18:27 -0600

    changed default colors

--- pychron/core/helpers/color_generators.py
+++ pychron/core/helpers/color_generators.py
@@ -52,8 +52,8 @@ for color in colors8i:
     c = colors8i[color]
     colors1f[color] = c[0] / 255., c[1] / 255., c[2] / 255.
 
-colornames = ['black', 'red', 'blue', 'violet', 'maroon',
-              'lime green', 'gray', 'green', 'aquamarine',
+colornames = ['black', 'lime green', 'blue', 'violet', 'maroon',
+              'red', 'gray', 'green', 'aquamarine',
               'silver', 'navy', 'plum', 'purple',
               'blue violet', 'brown', 'firebrick', 'green yellow',
 ]
