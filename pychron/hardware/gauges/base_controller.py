2d0abea7d850477977d5ab5c91c91077d9bd5bcc
Jake Ross <jirhiker@gmail.com>
2017-11-15 08:55:24 -0700

    added mks 937 controller

---
+++ pychron/hardware/gauges/base_controller.py
@@ -0,0 +1,114 @@
+# ===============================================================================
+# Copyright 2017 ross
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ===============================================================================
+from traits.api import HasTraits, List, Str, Float, Int
+
+
+class BaseGauge(HasTraits):
+    name = Str
+    pressure = Float
+    display_name = Str
+    low = 5e-10
+    high = 1e-8
+    color_scalar = 1
+    width = Int(100)
+    channel = Str
+
+
+class BaseGaugeController(HasTraits):
+    address = Str
+    gauges = List
+    display_name = Str
+    gauge_klass = BaseGauge
+
+    def get_gauge(self, name):
+        return next((gi for gi in self.gauges
+                     if gi.name == name or gi.display_name == name), None)
+
+    def get_pressure(self, name, force=False, verbose=False):
+        gauge = self.get_gauge(name)
+        if gauge is not None:
+            if force:
+                self._update_pressure(name, verbose)
+
+            return gauge.pressure
+
+    def _pressure_change(self, obj, name, old, new):
+        self.trait_set(**{'{}_pressure'.format(obj.name): new})
+
+    def _read_pressure(self, *args, **kw):
+        raise NotImplementedError
+
+    def _set_gauge_pressure(self, name, v):
+        g = self.get_gauge(name)
+        if g is not None:
+            try:
+                g.pressure = float(v)
+            except (TypeError, ValueError):
+                pass
+
+    def _get_pressure(self, name, verbose=False, force=False):
+        if self._scanning and not force:
+            attr = '{}_pressure'.format(name)
+            if hasattr(self, attr):
+                return getattr(self, attr)
+
+        return self._read_pressure(name, verbose)
+
+    def _update_pressure(self, name, verbose):
+        gauge = self.get_gauge(name)
+        if gauge:
+            p = self._read_pressure(name, verbose)
+            gauge.pressure = float(p)
+
+    def _load_gauges(self, config, *args, **kw):
+        ns = self.config_get(config, 'Gauges', 'names')
+        if ns:
+            ans = self.config_get(config, 'Gauges', 'display_names', optional=True)
+            if not ans:
+                ans = ns
+
+            lows = self.config_get(config, 'Gauges', 'lows', optional=True, default='1e-10, 1e-3, 1e-3')
+            highs = self.config_get(config, 'Gauges', 'highs', optional=True, default='1e-6, 1, 1')
+            cs = self.config_get(config, 'Gauges', 'color_scalars', optional=True, default='1, 1, 1')
+            chs = self.config_get(config, 'Gauges', 'channels', optional=True, default='1, 2, 3')
+
+            for gi in zip(*map(lambda x: x.split(','), (ns, ans, lows, highs, cs, chs))):
+                ni, ai, li, hi, ci, cn = map(str.strip, gi)
+
+                g = self.gauge_klass(name=ni, display_name=ai, channel=cn)
+                try:
+                    g.low = float(li)
+                except ValueError, e:
+                    self.warning_dialog('Invalid lows string. {}'.format(e), title=self.config_path)
+                    continue
+
+                try:
+                    g.high = float(hi)
+                except ValueError, e:
+                    self.warning_dialog('Invalid highs string. {}'.format(e), title=self.config_path)
+                    continue
+                try:
+                    g.color_scalar = int(ci)
+                except ValueError, e:
+                    self.warning_dialog('Invalid color_scalar string. {}'.format(e), title=self.config_path)
+                    continue
+
+                p = '{}_pressure'.format(ni)
+                self.add_trait(p, Float)
+                g.on_trait_change(self._pressure_change, 'pressure')
+
+                self.gauges.append(g)
+# ============= EOF =============================================
--- pychron/hardware/gauges/mks/controller.py
+++ pychron/hardware/gauges/mks/controller.py
@@ -1,5 +1,5 @@
 # ===============================================================================
-# Copyright 2011 Jake Ross
+# Copyright 2017 ross
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -13,18 +13,11 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # ===============================================================================
-# =============enthought library imports=======================
-from traits.api import List, Str, HasTraits, Float, Int
-from traitsui.api import View, HGroup, Item, ListEditor, InstanceEditor, Group
-# =============standard library imports ========================
-from numpy import random, char
-import time
+from traitsui.api import View, Item, HGroup, Group, ListEditor, InstanceEditor
 
-# =============local library imports  ==========================
-from pychron.hardware.core.core_device import CoreDevice
 from pychron.core.ui.color_map_bar_editor import BarGaugeEditor
-from pychron.hardware.gauges.base_controller import BaseGauge
-from pychron.hardware.gauges.granville_phillips.base_micro_ion_controller import BaseMicroIonController
+from pychron.hardware.core.core_device import CoreDevice
+from pychron.hardware.gauges.base_controller import BaseGauge, BaseGaugeController
 
 
 class Gauge(BaseGauge):
@@ -46,9 +39,28 @@ class Gauge(BaseGauge):
         return v
 
 
-class MicroIonController(BaseMicroIonController, CoreDevice):
-    scan_func = 'get_pressures'
+class MKSController(BaseGaugeController, CoreDevice):
     gauge_klass = Gauge
+
+    def _read_pressure(self, name, verbose=False):
+        g = self.get_gauge(name)
+        cmd = self._build_command('PR{}'.format(g.channel))
+        r = self.ask(cmd)
+
+        try:
+            return float(r)
+        except ValueError:
+            pass
+
+    def _build_command(self, cmd):
+        return '@{}{};FF'.format(self.address, cmd)
+
+    def load_additional_args(self, config, *args, **kw):
+        self.address = self.config_get(config, 'General', 'address', optional=False)
+        self.display_name = self.config_get(config, 'General', 'display_name', default=self.name)
+        # self.mode = self.config_get(config, 'Communications', 'mode', default='rs485')
+        self._load_gauges(config)
+        return True
 
     def gauge_view(self):
         v = View(Group(Item('gauges', style='custom',
@@ -60,15 +72,4 @@ class MicroIonController(BaseMicroIonController, CoreDevice):
                        label=self.display_name))
         return v
 
-    def graph_builder(self, g):
-        super(MicroIonController, self).graph_builder(g, show_legend=True)
-        g.new_series()
-        g.set_series_label('IG', series=0)
-
-        g.new_series()
-        g.set_series_label('CG1', series=1)
-
-        g.new_series()
-        g.set_series_label('CG2', series=2)
-
-# ============= EOF ====================================
+# ============= EOF =============================================
