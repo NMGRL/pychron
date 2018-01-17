0a213ef27368bcf2d0aa3daf4bd55cebd12fd4a2
Jake Ross <jirhiker@gmail.com>
2018-01-16 21:36:22 -0700

    added model335

--- pychron/hardware/lakeshore/base_controller.py
+++ pychron/hardware/lakeshore/base_controller.py
@@ -13,6 +13,44 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # ===============================================================================
+from traits.api import Enum
+from pychron.hardware import get_float
+from pychron.hardware.core.core_device import CoreDevice
+import re
 
+IDN_RE = re.compile(r'\w{4},\w{8},\w{7}\/\w{7},\d.\d')
+
+
+class BaseLakeShoreController(CoreDevice):
+    units = Enum('C', 'K')
+
+    def load_additional_args(self, config):
+        self.set_attribute(config, 'units', 'General', 'units', default='C')
+        return True
+
+    def initialize(self, *args, **kw):
+        self.communicator.write_terminator = chr(10)  # line feed \n
+
+    def test_connection(self):
+        self.tell('*CLS')
+        resp = self.ask('*IDN?')
+        return bool(IDN_RE.match(resp))
+
+    @get_float
+    def read_setpoint(self, output):
+        return self.ask('SETP? {}'.format(output))
+
+    def set_setpoint(self, output, v):
+        self.tell('SETP {},{}'.format(output, v))
+
+    def read_input_a(self):
+        return self._read_input('a', self.units)
+
+    def read_input_b(self):
+        return self._read_input('b', self.units)
+
+    @get_float
+    def _read_input(self, tag, mode='C'):
+        return self.ask('{}RDG? {}'.format(mode, tag))
 
 # ============= EOF =============================================
