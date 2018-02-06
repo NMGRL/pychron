fc475728d426b2dcbc8e3fc22f94dd13694cc99f
Jake Ross <jirhiker@gmail.com>
2018-02-05 17:08:19 -0700

    updated wisc_ar

--- pychron/data_mapper/sources/wiscar_source.py
+++ pychron/data_mapper/sources/wiscar_source.py
@@ -13,9 +13,48 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # ===============================================================================
+import xlrd
+import os
+from traits.api import File
 from pychron.data_mapper.sources.nu_source import NuFileSource
 
+DATA_FILE_COL = 1
+SAMPLE_COL = 5
+MATERIAL_COL = 7
+
+J_COL = 1
+JERR_COL = 2
+IRRADIATION_COL = 18
 
 class WiscArNuSource(NuFileSource):
-    pass
+    metadata_path = File
+
+    def get_analysis_import_spec(self, delimiter=None):
+        spec = super(WiscArNuSource, self).get_analysis_import_spec(delimiter=delimiter)
+
+        # load the metadata located in an xls file
+        wb = xlrd.open_workbook(self.metadata_path)
+        datasheet = wb.sheet_by_name('Data')
+
+        name = os.path.basename(self.path)
+        r = datasheet.row(0)
+        spec.run_spec.irradiation = r[IRRADIATION_COL].value
+        spec.run_spec.irradiation_level = 'A'
+
+        for row in datasheet.get_rows():
+            if row[0].value == 'J':
+                spec.j = float(row[J_COL].value)
+                spec.j_err = float(row[JERR_COL].value)
+
+            if row[DATA_FILE_COL].value == name:
+                break
+
+        else:
+            return spec
+
+        spec.run_spec.sample = row[SAMPLE_COL].value
+        spec.run_spec.material = row[MATERIAL_COL].value
+        spec.run_spec.project = 'NoProject'
+        return spec
+    
 # ============= EOF =============================================
---
+++ pychron/data_mapper/tests/wisc_ar_nu.py
@@ -0,0 +1,63 @@
+import unittest
+
+import os
+
+from pychron.data_mapper.sources.wiscar_source import WiscArNuSource
+from pychron.data_mapper.tests.file_source import BaseFileSourceTestCase
+from pychron.data_mapper.tests import fget_data_dir
+
+
+class WiscArNuTestCase(BaseFileSourceTestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.src = WiscArNuSource()
+        p = os.path.join(fget_data_dir(), 'Data_NAG1072.RUN')
+        pnice = os.path.join(fget_data_dir(), 'wiscar.nice')
+        mp = os.path.join(fget_data_dir(), 'H-15-27_C5_20170610.xls')
+        cls.src.path = p
+        cls.src.nice_path = pnice
+        cls.src.metadata_path = mp
+        cls.spec = cls.src.get_analysis_import_spec()
+
+        cls.expected = {'runid': 'Data_NAG1072',
+                        'irradiation': 'UW133',
+                        'irradiation_level': 'A',
+                        'sample': 'Blank 1',
+                        'material': 'Blank',
+                        'project': 'NoProject',
+                        'j': 0.000811,
+                        'j_err': 0.0000024,
+                        'timestamp_month': 6,
+                        'cnt40': 200,
+                        'cnt39': 200,
+                        'cnt38': 200,
+                        'cnt37': 200,
+                        'cnt36': 200,
+                        'discrimination': 1.0462399093612802,
+                        'uuid': 'Data_NAG1072'
+                        }
+
+    def test_irradiation(self):
+        self.assertEqual(self.spec.run_spec.irradiation, self.expected['irradiation'])
+
+    def test_level(self):
+        self.assertEqual(self.spec.run_spec.irradiation_level, self.expected['irradiation_level'])
+
+    def test_sample(self):
+        self.assertEqual(self.spec.run_spec.sample, self.expected['sample'])
+
+    def test_material(self):
+        self.assertEqual(self.spec.run_spec.material, self.expected['material'])
+
+    def test_project(self):
+        self.assertEqual(self.spec.run_spec.project, self.expected['project'])
+
+    def test_j(self):
+        self.assertEqual(self.spec.j, self.expected['j'])
+
+    def test_j_err(self):
+        self.assertEqual(self.spec.j_err, self.expected['j_err'])
+
+
+if __name__ == '__main__':
+    unittest.main()
