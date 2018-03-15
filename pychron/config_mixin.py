e58ee3d016129e2143fc946d184b1d547d1ed41f
Jake Ross <jirhiker@gmail.com>
2018-03-15 09:45:39 -0600

    fixing travis

--- pychron/config_mixin.py
+++ pychron/config_mixin.py
@@ -15,7 +15,6 @@
 # ===============================================================================
 
 # ============= enthought library imports =======================
-from __future__ import absolute_import
 from configparser import ConfigParser, RawConfigParser
 import os
 # ============= standard library imports ========================
