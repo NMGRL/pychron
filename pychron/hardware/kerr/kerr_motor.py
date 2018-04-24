7341afa32620793f3a23a60b85f59042098152cd
jirhiker <jirhiker@gmail.com>
2018-04-24 09:44:51 -0600

    kerr motor tweak

--- pychron/hardware/kerr/kerr_motor.py
+++ pychron/hardware/kerr/kerr_motor.py
@@ -262,6 +262,10 @@ class KerrMotor(KerrDevice, ConsumerMixin):
                                nbytes=3,
                                verbose=verbose,
                                info='get status byte')
+
+        sb = ''
+        self.debug('status_byte = {}'.format(sb))
+
         return status_byte
 
     def read_defined_status(self, verbose=True):
@@ -274,6 +278,9 @@ class KerrMotor(KerrDevice, ConsumerMixin):
                                nbytes=2,
                                info='get defined status',
                                verbose=verbose)
+
+        status_register = map(int, make_bitarray(int(status_byte[:2], 16)))
+        self.debug('Defined Status Byte={}'.format(status_register))
         return status_byte
 
     def read_home_position(self):
