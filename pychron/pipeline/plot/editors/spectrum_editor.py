69de3c6a7c63ff269988f57602db7a6498b51801
Jake Ross <jirhiker@gmail.com>
2018-07-20 13:21:06 -0600

    addressed #1599

--- pychron/pipeline/plot/editors/spectrum_editor.py
+++ pychron/pipeline/plot/editors/spectrum_editor.py
@@ -15,8 +15,6 @@
 # ===============================================================================
 
 # ============= enthought library imports =======================
-
-from __future__ import absolute_import
 from traits.api import Instance
 
 # ============= standard library imports ========================
@@ -24,11 +22,6 @@ from traits.api import Instance
 from pychron.options.options_manager import SpectrumOptionsManager
 from pychron.pipeline.plot.editors.interpreted_age_editor import InterpretedAgeEditor
 from pychron.pipeline.plot.models.spectrum_model import SpectrumModel
-from pychron.processing.analyses.file_analysis import SpectrumFileAnalysis
-import six
-
-
-# from zobs.options.plotter_options_manager import SpectrumOptionsManager
 
 
 class SpectrumEditor(InterpretedAgeEditor):
@@ -43,55 +36,54 @@ class SpectrumEditor(InterpretedAgeEditor):
             else:
                 ia.preferred_age_kind = 'Integrated'
 
-    # def get_component(self, ans, plotter_options):
-    # if plotter_options is None:
-    #         pom = SpectrumOptionsManager()
-    #         plotter_options = pom.plotter_options
-    #
-    #     from pychron.processing.plotters.spectrum.spectrum_model import SpectrumModel
-    #
-    #     # c = u'Plateau age calculated as weighted mean of plateau steps. ' \
-    #     #     u'Integrated age calculated as isotopic recombination of all steps.\n' \
-    #     #     u'Plateau and Integrated Age uncertainties \u00b1{}\u03c3.' \
-    #     #     u'GMC=Groundmass Concentrate, Kaer=Kaersutite, Plag=Plagioclase'
-    #     #
-    #     # self._add_caption(component, plotter_options, default_captext=c)
-    #     model, component = self._make_component(SpectrumModel, ans, plotter_options)
-    #     return model, component
+# ============= EOF =============================================
+# def get_component(self, ans, plotter_options):
+# if plotter_options is None:
+#         pom = SpectrumOptionsManager()
+#         plotter_options = pom.plotter_options
+#
+#     from pychron.processing.plotters.spectrum.spectrum_model import SpectrumModel
+#
+#     # c = u'Plateau age calculated as weighted mean of plateau steps. ' \
+#     #     u'Integrated age calculated as isotopic recombination of all steps.\n' \
+#     #     u'Plateau and Integrated Age uncertainties \u00b1{}\u03c3.' \
+#     #     u'GMC=Groundmass Concentrate, Kaer=Kaersutite, Plag=Plagioclase'
+#     #
+#     # self._add_caption(component, plotter_options, default_captext=c)
+#     model, component = self._make_component(SpectrumModel, ans, plotter_options)
+#     return model, component
 
-    def _check_for_necessary_attributes(self, d):
-        ms = [k for k in ['age', 'age_err', 'k39']
-              if k not in d]
-        return ms
-
-    def _get_items_from_file(self, parser):
-        ans = []
-        for i, d in enumerate(parser.itervalues()):
-            missing_keys = self._check_for_necessary_attributes(d)
-            if not missing_keys:
-                f = SpectrumFileAnalysis(age=float(d['age']),
-                                         age_err=float(d['age_err']),
-                                         k39_value=float(d['k39']),
-                                         record_id=d.get('runid', ''),
-                                         sample=d.get('sample', ''))
-                ans.append(f)
-            else:
-                self.warning('Invalid analysis. Number = {}. Missing Keys={}'.format(i, ','.join(missing_keys)))
-
-                # ans = [construct(args)
-                #        for args in par.itervalues()]
-
-        po = self.plotter_options_manager.plotter_options
-        for ap in po.aux_plots:
-            if ap.name.lower() not in ('age',):
-                ap.use = False
-                ap.enabled = False
-            # clear overlay positions
-            ap.overlay_positions = {}
-
-        return ans
+# def _check_for_necessary_attributes(self, d):
+#     ms = [k for k in ['age', 'age_err', 'k39']
+#           if k not in d]
+#     return ms
+#
+# def _get_items_from_file(self, parser):
+#     ans = []
+#     for i, d in enumerate(parser.itervalues()):
+#         missing_keys = self._check_for_necessary_attributes(d)
+#         if not missing_keys:
+#             f = SpectrumFileAnalysis(age=float(d['age']),
+#                                      age_err=float(d['age_err']),
+#                                      k39_value=float(d['k39']),
+#                                      record_id=d.get('runid', ''),
+#                                      sample=d.get('sample', ''))
+#             ans.append(f)
+#         else:
+#             self.warning('Invalid analysis. Number = {}. Missing Keys={}'.format(i, ','.join(missing_keys)))
+#
+#             # ans = [construct(args)
+#             #        for args in par.itervalues()]
+#
+#     po = self.plotter_options_manager.plotter_options
+#     for ap in po.aux_plots:
+#         if ap.name.lower() not in ('age',):
+#             ap.use = False
+#             ap.enabled = False
+#         # clear overlay positions
+#         ap.overlay_positions = {}
+#
+#     return ans
 
 # class AutoSpectrumEditor(SpectrumEditor):
 #    auto_figure_control = Instance(AutoSpectrumControl, ())
-
-# ============= EOF =============================================
