47e2cd3c87e6eccf0ef7af2addb579db6443201b
Jake Ross <jirhiker@gmail.com>
2018-09-05 16:59:52 -0600

    added model spectrum and model arrhenius

--- pychron/mdd/tasks/mdd_figure.py
+++ pychron/mdd/tasks/mdd_figure.py
@@ -26,6 +26,16 @@ from pychron.pychron_constants import NULL_STR
 PANELS = [NULL_STR, 'Arrhenius', 'LogR Ro', 'Spectrum', 'Cooling History']
 
 
+def make_panel(p):
+    if p == 'Arrhenius':
+        ret = ('Arrhenius', 'Model Arrhenius')
+    elif p == 'Spectrum':
+        ret = ('Spectrum', 'Model Spectrum')
+    else:
+        ret = (p,)
+    return ret
+
+
 class MDDFigureOptions(FigureOptions):
     subview_names = List(['Main', 'Appearance'])
 
@@ -36,7 +46,7 @@ class MDDFigureOptions(FigureOptions):
 
     def panels(self):
         ps = self.panel_ul, self.panel_ur, self.panel_ll, self.panel_lr
-        return [(i, p) for i, p in enumerate(ps) if not p == NULL_STR]
+        return [(i, make_panel(p)) for i, p in enumerate(ps) if not p == NULL_STR]
 
     def rc(self):
         kind, n, r, c = 'g', 4, 2, 2
@@ -45,16 +55,16 @@ class MDDFigureOptions(FigureOptions):
                 (self.panel_ll == NULL_STR and self.panel_lr == NULL_STR):
             r = 1
             kind = 'h'
-            n=2
+            n = 2
 
         if self.panel_ul == NULL_STR and self.panel_ll == NULL_STR or \
                 self.panel_ur == NULL_STR and self.panel_lr == NULL_STR or \
                 self.panel_ur == NULL_STR and self.panel_ll == NULL_STR:
             c = 1
             kind = 'v'
-            n=2
+            n = 2
 
-        return kind, n,r, c
+        return kind, n, r, c
 
     def _get_subview(self, name):
         return VIEWS[name]
