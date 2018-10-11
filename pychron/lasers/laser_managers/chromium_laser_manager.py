e926793cf664fc5196553e1d22a99374c8276ef8
Jake Ross <jirhiker@gmail.com>
2018-10-11 14:33:05 -0600

    chromium updates for patterning

--- pychron/lasers/laser_managers/chromium_laser_manager.py
+++ pychron/lasers/laser_managers/chromium_laser_manager.py
@@ -83,7 +83,6 @@ class ChromiumLaserManager(EthernetLaserManager):
     def get_position(self):
         x, y, z = self._x, self._y, self._z
         xyz_microns = self.ask('stage.pos?\n')
-        print('fsad', xyz_microns, type(xyz_microns))
         if xyz_microns:
             x, y, z = [float(v) / 1000. for v in xyz_microns.split(',')]
             x = x * self.stage_manager.x_sign
@@ -157,7 +156,7 @@ class ChromiumLaserManager(EthernetLaserManager):
             except ValueError as e:
                 print('_moving exception {}'.format(e))
 
-        r = self._block(cmd='stage.pos?\n', cmpfunc=cmpfunc)
+        self._block(cmd='stage.pos?\n', cmpfunc=cmpfunc)
         time.sleep(0.25)
         self._alive = False
         self.update_position()
@@ -181,7 +180,6 @@ class ChromiumLaserManager(EthernetLaserManager):
                      self.stage_manager.z_sign * z * 1000
 
         cmd = 'stage.moveto {:0.0f},{:0.0f},{:0.0f},{:0.0f},{:0.0f},{:0.0f}'.format(xm, ym, zm, xs, ys, zs)
-        print('cmd is ', cmd)
         self.info('sending {}'.format(cmd))
         self.ask(cmd)
 
