706f4f6de42a508a6c0d10d0d0ae00d5827e3879
Jake Ross <jirhiker@gmail.com>
2019-01-30 09:28:09 -0700

    added display_uuid to analysestbl

--- pychron/dvc/dvc_orm.py
+++ pychron/dvc/dvc_orm.py
@@ -240,6 +240,13 @@ class AnalysisTbl(Base, IDMixin):
         if self.repository_associations and len(self.repository_associations) == 1:
             return self.repository_associations[0].repository
 
+    @property
+    def display_uuid(self):
+        u = self.uuid
+        if not u:
+            u = ''
+        return u[:8]
+
     def get_load_name(self):
         ln = ''
         if self.measured_positions:
