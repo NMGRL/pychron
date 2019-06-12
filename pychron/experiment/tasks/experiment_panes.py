b89ce756029de9326c0a60b276a7a526e5196837
nmgrluser <jan@argonlab7s-Mac-mini.local>
2019-06-12 15:15:18 -0600

    added recalculate to stats pane

--- pychron/experiment/tasks/experiment_panes.py
+++ pychron/experiment/tasks/experiment_panes.py
@@ -328,9 +328,14 @@ class ConnectionStatusPane(TraitsDockPane):
 class StatsPane(TraitsDockPane):
     id = 'pychron.experiment.stats'
     name = 'Stats'
+    recalculate_button = Button('Recalculate')
+
+    def _recalculate_button_fired(self):
+        self.model.reset()
 
     def traits_view(self):
-        gen_grp = BorderVGroup(HGroup(Readonly('nruns', width=150, label='Total Runs'),
+        gen_grp = BorderVGroup(HGroup(UItem('pane.recalculate_button')),
+                               HGroup(Readonly('nruns', width=150, label='Total Runs'),
                                       UReadonly('total_time')),
                                HGroup(Readonly('nruns_finished', width=150, label='Completed'),
                                       UReadonly('elapsed')),
