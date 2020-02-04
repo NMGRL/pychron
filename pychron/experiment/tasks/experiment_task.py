d99648c18089e189fe6416457400dc88d92d87b8
Jake Ross <jirhiker@gmail.com>
2020-02-03 17:45:24 -0700

    fixed import globalv

--- pychron/experiment/tasks/experiment_task.py
+++ pychron/experiment/tasks/experiment_task.py
@@ -37,6 +37,7 @@ from pychron.experiment.tasks.experiment_panes import ExperimentFactoryPane, Sta
     ControlsPane, IsotopeEvolutionPane, ConnectionStatusPane, LoggerPane, ExplanationPane
 from pychron.experiment.utilities.identifier import convert_extract_device, is_special
 from pychron.experiment.utilities.save_dialog import ExperimentSaveDialog
+from pychron.globals import globalv
 from pychron.lasers.laser_managers.ilaser_manager import ILaserManager
 from pychron.paths import paths
 from pychron.pipeline.plot.editors.figure_editor import FigureEditor
