e8ba810e15087703d0c15f4dceb8b5f953cf214d
mjz1983 <mjz1983@users.noreply.github.com>
2023-07-18 16:07:43 +0000

    Formatting changes

--- pychron/graph/tools/info_inspector.py
+++ pychron/graph/tools/info_inspector.py
@@ -88,7 +88,7 @@ class InfoInspector(ScatterInspector):
             return
 
         if isinstance(pos, (tuple, list)):
-            print(pos, 'asdfsdfsdf')
+            print(pos, "asdfsdfsdf")
             try:
                 self.current_position = (pos[0][0], pos[1][0])
             except IndexError:
