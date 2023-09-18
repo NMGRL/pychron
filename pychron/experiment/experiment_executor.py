1bece5ad87c33e4a41d1041e57c819a3826bfecd
jross <jake.ross@nmt.edu>
2023-09-18 11:20:02 -0600

    misc

--- pychron/experiment/experiment_executor.py
+++ pychron/experiment/experiment_executor.py
@@ -329,7 +329,6 @@ class ExperimentExecutor(Consoleable, PreferenceMixin):
             "use_xls_persistence",
             "use_db_persistence",
             "experiment_type",
-            "laboratory",
             "ratio_change_detection_enabled",
             "use_preceding_blank",
             "execute_open_queues",
@@ -368,7 +367,7 @@ class ExperimentExecutor(Consoleable, PreferenceMixin):
         self._preference_binder(prefid, ("use_message_colormapping",))
 
         # general
-        self._preference_binder("pychron.general", ("default_principal_investigator",))
+        self._preference_binder("pychron.general", ("default_principal_investigator", "laboratory"))
 
     def add_event(self, *events):
         self.events.extend(events)
