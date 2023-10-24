bfd061b8cd9b4df2e63174e08f8f5b51ee53fe00
jakeross <jirhiker@gmail.com>
2023-10-24 17:16:28 -0600

    set laser power 0 on enable

--- pychron/hardware/uc2000.py
+++ pychron/hardware/uc2000.py
@@ -110,8 +110,12 @@ class UC2000(CoreDevice):
         return f"{ones_compliment:02x}"
 
     def _enable_laser(self, **kw):
+        self.set_laser_power(0)
+
         cmd = "75"
-        return is_ack(self._ask(cmd))
+        resp = is_ack(self._ask(cmd))
+        self.set_laser_power(0)
+        return resp
 
     def _disable_laser(self):
 
