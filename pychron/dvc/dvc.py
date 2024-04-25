4c50a483a088f37905ad3ae1acff7ded8c3e16ef
jirhiker <jirhiker@users.noreply.github.com>
2024-04-25 22:50:15 +0000

    Formatting changes

--- pychron/dvc/dvc.py
+++ pychron/dvc/dvc.py
@@ -1469,9 +1469,9 @@ class DVC(Loggable):
 
             # merge any new commits on the data_collection branch to this branch
             try:
-                repo.active_repo.git.checkout('origin/data_collection', '.')
-                repo.active_repo.git.add('.')
-                repo.active_repo.git.commit('-m', 'Merge origin/data_collection branch')
+                repo.active_repo.git.checkout("origin/data_collection", ".")
+                repo.active_repo.git.add(".")
+                repo.active_repo.git.commit("-m", "Merge origin/data_collection branch")
                 # repo.merge("origin/data_collection", inform=False)
             except BaseException:
                 self.debug_exception()
