7d82a0c4ffa297ebde234079abf94f7ef9106faa
jross <jake.ross@nmt.edu>
2024-09-24 11:22:00 -0600

    fixed is clean

--- pychron/git_archive/repo_manager.py
+++ pychron/git_archive/repo_manager.py
@@ -1022,7 +1022,10 @@ class GitRepoManager(Loggable):
         self.debug("ahead behind")
 
         repo = self._repo
-        ahead, behind = ahead_behind(repo, remote)
+        if not self._get_remote(remote):
+            ahead, behind = 0,0
+        else:
+            ahead, behind = ahead_behind(repo, remote)
 
         return ahead, behind
 
