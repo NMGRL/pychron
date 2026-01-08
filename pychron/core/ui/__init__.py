d8333cd8eeaa8a6c6478c2b0f303154cd72ba2c8
jross <jake.ross@nmt.edu>
2026-01-08 16:43:47 -0700

    fix: add compatibility patch for QDialog exec method in __init__.py

--- pychron/core/ui/__init__.py
+++ pychron/core/ui/__init__.py
@@ -7,5 +7,19 @@ def set_toolkit(name):
     ETSConfig.toolkit = name
 
 
+def _patch_qdialog_exec():
+    try:
+        from pyface.qt.QtWidgets import QDialog
+    except Exception:
+        try:
+            from pyface.qt.QtGui import QDialog
+        except Exception:
+            return
+
+    if not hasattr(QDialog, "exec_") and hasattr(QDialog, "exec"):
+        QDialog.exec_ = QDialog.exec
+
+
 def set_qt():
     set_toolkit("qt4")
+    _patch_qdialog_exec()
